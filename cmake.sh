890f4eb0c217751b9ea883260ecd2700fee8cbda
hinata <hinatachikao1971@gmail.com>
2025-01-28 15:20:30 +0330

    migrate from eclipse to vscode

--- cmake.sh
+++ cmake.sh
@@ -4,7 +4,7 @@
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 if [[ $1 == 0 ]]; then
-    nice -n 20 ./build/cpp_std20
+    nice -n 20 ./build/cpp_std_20
 else
   nice -n 20 cmake -B build -S .
   nice -n 20 cmake --build build --config debug --clean-first
@@ -12,11 +12,11 @@ else
   nice -n 20 printf '\n'
   #nice -n 20 echo "PRESS ANY KEY TO EXECUTE PROGRAM ..."
   #read
-  nice -n 20 ls -lthra ./build/cpp_std20
+  nice -n 20 ls -lthra ./build/cpp_std_20
   nice -n 20 printf '\n'
   nice -n 20 printf '%s\n' "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   nice -n 20 printf ""
-  nice -n 20 ./build/cpp_std20
+  nice -n 20 ./build/cpp_std_20
   nice -n 20 printf '\n'
   nice -n 20 printf '%s\n' "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 fi
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,6 +4,12 @@
 
 int main() {
 
+    int score[]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int *p_score{score};
+
+    std::cout << "score is  : " << score << nl;
+    std::cout << "p_score is: " << p_score << nl;
+
 
     std::cout << "\n #STD20(20:15:26): The End ..." << std::endl;
     return (EXIT_SUCCESS);
