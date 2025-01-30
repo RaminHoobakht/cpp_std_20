9febb307423ccfe2363e2efccd5d6b5682bad6a7
hinata <hinatachikao1971@gmail.com>
2025-01-31 03:20:08 +0330

    before creating new source file

--- src/header/main.hpp
+++ src/header/main.hpp
@@ -1,8 +1,25 @@
 #ifndef HEADER_MAIN_CPP
 #define HEADER_MAIN_CPP
 
+#include <iostream>
+
 #define NL '\n'
 #define SP '\x20'
+
+
+namespace util {
+
+    void separator(size_t no, char ch) noexcept;
+
+    void separator(const size_t no, const char ch) noexcept {
+        for (size_t i{0LLU}; i < no; ++i) {
+            std::cout << ch;
+        }
+        std::cout << NL;
+    }
+
+
+} // namespace util
 
 
 #endif /* HEADER_MAIN_CPP */
