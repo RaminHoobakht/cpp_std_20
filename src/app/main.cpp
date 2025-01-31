1e2ac3b1f333a5a6c9ac6092cdf8a5f7aa72c414
hinata <hinatachikao1971@gmail.com>
2025-02-01 01:48:51 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    int array[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int *const p_array{array};
 
+    /* p_array can not change it's pointer because
+       it restricted by const keyword. so, this source
+       encounter with compile time error. */
+
+    p_array++;
+    std::cout << *(p_array) << NL;
+
+    p_array++;
+    std::cout << *(p_array) << NL;
+
+    p_array++;
+    std::cout << *(p_array) << NL;
 
     std::cout << "\n #(01:39:57): The End ..." << std::endl;
     return EXIT_SUCCESS;
