50229383e319a6e500bc296055f89c9006212748
hinata <hinatachikao1971@gmail.com>
2025-02-01 01:39:58 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,22 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
+
 
 int main() {
 
-    /* array can not increment or decrement 
-       because it's pointer is const. */
-    int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int result{};
-
-    std::cout << *(arr + 0) << NL;
-
-    result = *(arr + 1);
-    std::cout << result << NL;
-
-    result = *(arr + 2);
-    std::cout << result << NL;
 
 
-    std::cout << "\n #(09:09:53): The End ..." << std::endl;
+    std::cout << "\n #(01:39:57): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
