03d4dd0c43f45eea5a705c21b83f60a845b958d1
hinata <hinatachikao1971@gmail.com>
2025-01-31 03:38:05 +0330

    new source file

---
+++ archive/src_004/header/main.hpp
@@ -0,0 +1,8 @@
+#ifndef HEADER_MAIN_CPP
+#define HEADER_MAIN_CPP
+
+#define NL '\n'
+#define SP '\x20'
+
+
+#endif /* HEADER_MAIN_CPP */
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,27 +4,8 @@
 
 int main() {
 
-    constexpr size_t buff_size{10LLU};
-    const int scores[buff_size]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    const int *p_scores{scores};
 
-    std::cout << "address: " << p_scores << " value: " << *(p_scores) << NL;
 
-    ++p_scores;
-    std::cout << "address: " << p_scores << " value: " << *(p_scores) << NL;
-
-    ++p_scores;
-    std::cout << "address: " << p_scores << " value: " << *(p_scores) << NL;
-
-    ++p_scores;
-    std::cout << "address: " << p_scores << " value: " << *(p_scores) << NL;
-
-    ++p_scores;
-    std::cout << "address: " << p_scores << " value: " << *(p_scores) << NL;
-
-    ++p_scores;
-    std::cout << "address: " << p_scores << " value: " << *(p_scores) << NL;
-
-    std::cout << "\n #(03:20:08): The End ..." << std::endl;
+    std::cout << "\n #(03:38:05): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
