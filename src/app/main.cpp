82253a4e5c40b684feecb8cc5440e5f58b7299ab
hinata <hinatachikao1971@gmail.com>
2025-01-31 09:09:53 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,40 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    constexpr size_t min{0LLU};
-    int temp{};
-    int arr[]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
 
-    size_t length{std::size(arr)};
-    size_t opr{(length / 2)};
 
-    length % 2 ? opr++ : opr--;
-
-    util::separator();
-
-    for (size_t i{min}; i < length; ++i) {
-        std::cout << *(arr + i) << '\x20';
-    }
-    std::cout << NL;
-
-    util::separator();
-
-    for (size_t first{min}, last{length - 1}; first <= opr; ++first, last--) {
-        temp = *(arr + first);
-        *(arr + first) = *(arr + last);
-        *(arr + last) = temp;
-    }
-
-    for (size_t i{min}; i < length; ++i) {
-        std::cout << *(arr + i) << '\x20';
-    }
-    std::cout << NL;
-
-    util::separator();
-
-    std::cout << "\n #(08:48:46): The End ..." << std::endl;
+    std::cout << "\n #(09:09:53): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
