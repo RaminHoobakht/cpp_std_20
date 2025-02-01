2c2211e204f2741ba2d0c620d3875efdfc03e8fb
hinata <hinatachikao1971@gmail.com>
2025-02-02 01:49:09 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,25 +1,11 @@
-/* dangling pointer: multiple pointer points to the same address */
-
 #include <iostream>
 #include "../header/main.hpp"
 
 
 int main() {
 
-    int *ptr_number1{new int{120}};
-    int *ptr_number2{ptr_number1};
 
-    util::separator();
-    std::cout << "address of ptr_number1 is: " << ptr_number1 << NL;
-    std::cout << "address of ptr_number2 is: " << ptr_number2 << NL;
 
-    util::separator();
-    std::cout << "value of ptr_number1 is: " << *ptr_number1 << NL;
-    std::cout << "value of ptr_number2 is: " << *ptr_number2 << NL;
-
-    delete ptr_number1;
-    delete ptr_number2; /* encounter with compile time error */
-
-    std::cout << "\n #(01:43:31): The End ..." << std::endl;
+    std::cout << "\n #(01:49:09): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
