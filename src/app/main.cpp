384f96d8da4ec2307345ea724d9c221ba5e66ec4
hinata <hinatachikao1971@gmail.com>
2025-02-02 01:43:30 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
+/* dangling pointer: deleted pointer */
+
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    int *ptr_number{new int{120}};
 
+    std::cout << "address of the ptr_number is: " << ptr_number << NL;
+    std::cout << "value of ptr_number is      : " << *ptr_number << NL;
+    util::separator();
+
+    delete ptr_number;
+
+    /* junk pointer */
+    std::cout << "address of the ptr_number is: " << ptr_number << NL;
+    std::cout << "value of ptr_number is      : " << *ptr_number << NL;
+    util::separator();
 
     std::cout << "\n #(21:59:11): The End ..." << std::endl;
     return EXIT_SUCCESS;
