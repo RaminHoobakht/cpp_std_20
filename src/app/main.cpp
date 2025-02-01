f8fe0aa4f88d49c9afbe8ea000902f20eea2e12a
hinata <hinatachikao1971@gmail.com>
2025-02-01 21:59:11 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,18 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    /* dynamic allocate int size memory and assign value directly in one line */
-    int *ptr_number{new int{55}};
 
-    /* print the value of the ptr_number pointer variable */
-    std::cout << " value of the ptr_number is: " << *ptr_number << NL;
 
-    /* remove allocated space of ptr_number from the heap memory */
-    delete ptr_number;
-
-    std::cout << "\n #(21:54:14): The End ..." << std::endl;
+    std::cout << "\n #(21:59:11): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
