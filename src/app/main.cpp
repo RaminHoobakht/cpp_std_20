b5cbebc84c75f6ca4ae1e2d41a621c339bfee7fc
hinata <hinatachikao1971@gmail.com>
2025-02-01 21:54:14 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,21 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    /* create a pointer that points to the nullptr */
-    int *ptr_number{nullptr};
 
-    /* allocate int size memory on the heap */
-    ptr_number = new int;
 
-    /* assign an integer number  */
-    *ptr_number = 55;
-
-    /* print the value of the specific heap address memory */
-    std::cout << "value of ptr_number is: " << *ptr_number << NL;
-
-    std::cout << "\n #(19:34:02): The End ..." << std::endl;
+    std::cout << "\n #(21:54:14): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
