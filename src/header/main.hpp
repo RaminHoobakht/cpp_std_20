9f6906f49ea1027cdacd5ae23378482ae75a495a
hinata <hinatachikao1971@gmail.com>
2025-02-02 15:29:46 +0330

    before creating new source file

--- src/header/main.hpp
+++ src/header/main.hpp
@@ -1,6 +1,7 @@
 #ifndef HEADER_MAIN_CPP
 #define HEADER_MAIN_CPP
 
+#include <format>
 #include <iostream>
 
 #define NL '\n'
@@ -10,10 +11,21 @@ namespace util {
 
     constexpr size_t min{0LLU};
 
-    void separator(size_t, char) noexcept;
+    inline void separator(size_t, char) noexcept;
+    inline void separator_msg(const char *, size_t, char) noexcept;
 
 
-    void separator(const size_t no = 64, const char ch = '-') noexcept {
+    inline void separator(const size_t no = 64, const char ch = '-') noexcept {
+        for (size_t i{min}; i < no; ++i) {
+            std::cout << ch;
+        }
+        std::cout << NL;
+    }
+
+
+    inline void separator_msg(const char *msg, const size_t no = 16,
+                              const char ch = '-') noexcept {
+        std::cout << NL << ch << ch << ch << SP << msg << SP;
         for (size_t i{min}; i < no; ++i) {
             std::cout << ch;
         }
