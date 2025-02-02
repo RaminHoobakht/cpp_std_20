f399d4641d20d5804b7bccc1d6a4e331bbe398fe
hinata <hinatachikao1971@gmail.com>
2025-02-02 04:12:19 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,8 +3,16 @@
 
 
 int main() {
+    const auto *heap_number{new int{120}};
 
+    std::cout << "value of the heap_number is: " << *heap_number << NL;
 
+    heap_number = new int{700};
+    util::separator();
+    std::cout << "value of the heap_number is: " << *heap_number << NL;
+
+    delete heap_number;
+    heap_number = nullptr;
 
     std::cout << "\n #(03:51:06): The End ..." << std::endl;
     return EXIT_SUCCESS;
