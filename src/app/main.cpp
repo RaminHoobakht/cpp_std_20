e4f742654eef09d8185ee3f9c1702e4504b4ee6e
hinata <hinatachikao1971@gmail.com>
2025-02-02 03:35:55 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,8 +3,13 @@
 
 
 int main() {
+    constexpr size_t buffer_size{10'000'000'000'000LLU};
 
-
+    if (const auto array{new(std::nothrow) int[buffer_size]{}}; array == nullptr) {
+        std::cout << "allocate memory on heap is failed ..." << NL;
+    } else {
+        std::cout << "allocate memory on heap successfully ..." << NL;
+    }
 
     std::cout << "\n #(03:25:40): The End ..." << std::endl;
     return EXIT_SUCCESS;
