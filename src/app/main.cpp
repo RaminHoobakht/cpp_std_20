44a65f8856bfa988dcdb409df31b285c90fef5f6
hinata <hinatachikao1971@gmail.com>
2025-02-02 03:51:06 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
 
+    if (int *ptr_number{}; !(ptr_number == nullptr)) {
+        std::cout << " ptr_number has valid address ..." << NL;
+    } else {
+        std::cout << " ptr_number has invalid address ..." << NL;
+    }
+
+    util::separator();
+
+    if (int *ptr_number{new int{120}}; !(ptr_number == nullptr)) {
+        std::cout << " ptr_number has valid address ..." << NL;
+    } else {
+        std::cout << " ptr_number has invalid address ..." << NL;
+    }
 
     std::cout << "\n #(03:35:56): The End ..." << std::endl;
     return EXIT_SUCCESS;
