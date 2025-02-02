d2bfb1a5437ea24055f9f31c855e4d1780e28cdd
hinata <hinatachikao1971@gmail.com>
2025-02-02 09:35:24 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    double double_value{12.34};
+    double &ref_double_value{double_value};
+
+    double other_double_value{100.13};
+
+    ref_double_value = other_double_value;
+
+    std::cout << "double_value    : " << double_value << NL;
+    std::cout << "ref_double_value: " << ref_double_value << NL;
+    std::cout << "ref_double_value: " << ref_double_value << NL;
+
+    ref_double_value = 33.333;
+
+    util::separator();
+    std::cout << "double_value    : " << double_value << NL;
+    std::cout << "ref_double_value: " << ref_double_value << NL;
+    std::cout << "ref_double_value: " << other_double_value << NL;
 
 
     std::cout << "\n #(08:21:52): The End ..." << std::endl;
