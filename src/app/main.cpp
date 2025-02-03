63e78bc2b41fda49beb9286150473e623638280b
hinata <hinatachikao1971@gmail.com>
2025-02-03 10:37:08 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,20 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    int int_value{650};
-    int &ref_int_value{int_value};
 
-    std::cout << "int_value    :" << int_value << NL;
-    std::cout << "ref_int_value:" << ref_int_value << NL;
 
-    ref_int_value++;
-
-    std::cout << "int_value    :" << int_value << NL;
-    std::cout << "ref_int_value:" << ref_int_value << NL;
-
-    std::cout << "\n #(15:29:46): The End ..." << std::endl;
+    std::cout << "\n #(10:37:08): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
