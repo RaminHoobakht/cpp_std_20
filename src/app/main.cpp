f7628979668ccbc48f4bfd490cdd6c140a719e4a
hinata <hinatachikao1971@gmail.com>
2025-02-03 22:00:31 +0330

    new cpp source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,8 +3,26 @@
 
 
 int main() {
+    /* code */
 
+    int scores[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    std::cout << "printing original values: " << NL;
+    for (const auto &item: scores) {
+        std::cout << item << SP;
+    }
+    std::cout << NL;
+
+    for (auto &item: scores) {
+        item += 10;
+    }
+    std::cout << NL;
+
+    std::cout << "printing modified values: " << NL;
+    for (const auto &item: scores) {
+        std::cout << item << SP;
+    }
+    std::cout << NL;
 
     std::cout << "\n #(21:15:27): The End ..." << std::endl;
     return EXIT_SUCCESS;
