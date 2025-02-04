09ffd3762e2c2e6b37a4654958fd97a96fcfb1d5
hinata <hinatachikao1971@gmail.com>
2025-02-04 07:47:45 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,29 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
-    /* code */
 
-    int scores[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    std::cout << "printing original values: " << NL;
-    for (const auto &item: scores) {
-        std::cout << item << SP;
-    }
-    std::cout << NL;
 
-    for (auto &item: scores) {
-        item += 10;
-    }
-    std::cout << NL;
-
-    std::cout << "printing modified values: " << NL;
-    for (const auto &item: scores) {
-        std::cout << item << SP;
-    }
-    std::cout << NL;
-
-    std::cout << "\n #(21:15:27): The End ..." << std::endl;
+    std::cout << "\n #(07:47:45): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
