fb491a7e5403a5db9ce6c27bdb56e53bcb7f9445
hinata <hinatachikao1971@gmail.com>
2025-02-08 04:52:37 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,7 +4,6 @@
 
 
 int main() {
-
     // constexpr auto *str{"Try not. Do or do not. There is no try."};
     constexpr auto *str{"Try not. Do or do not. There is no try."};
     constexpr char target{'.'};
