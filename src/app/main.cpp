0a4d9be5d1ea48401acd2c0a64a52fcb1447c3e4
hinata <hinatachikao1971@gmail.com>
2025-02-08 19:16:27 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
 #include "../header/main.hpp"
-
+#include <cstring>
+#include <iostream>
 
 int main() {
 
+    char str[]{"/home/hinata/workspace/cplusplus/vscode/cpp_std_20"};
 
+    char *output{std::strrchr(str, '/')};
+    if (output) {
+        std::cout << "result is: " << output + 1 << NL;
+    }
 
     std::cout << "\n #(18:39:25): The End ..." << std::endl;
     return EXIT_SUCCESS;
