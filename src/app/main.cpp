8feb68adc6ba8113cb8d73edb67757f099ff4e7a
hinata <hinatachikao1971@gmail.com>
2025-02-08 05:24:36 +0330

    new cpp source files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
-
-
+    /* code */
 
     std::cout << "\n #(04:52:37): The End ..." << std::endl;
     return EXIT_SUCCESS;
