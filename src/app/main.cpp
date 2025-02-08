c8fa46c82e44044aeb3347199b6e6d7028a7009d
hinata <hinatachikao1971@gmail.com>
2025-02-08 11:12:45 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
 #include "../header/main.hpp"
-
+#include <cstring>
+#include <iostream>
 
 int main() {
 
+    constexpr auto my_path{
+            "/home/hinata/workspace/cplusplus/vscode/cpp_std_20"};
+    constexpr auto target{'/'};
+    const char *result{my_path};
+
+    while ((result = std::strchr(result, target)) != nullptr) {
+        ++result;
+        std::cout << result << NL;
+    }
 
 
     std::cout << "\n #(10:54:06): The End ..." << std::endl;
