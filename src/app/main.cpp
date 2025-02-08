58ae74430243ba7dfa523d8870989882f69fdfb8
hinata <hinatachikao1971@gmail.com>
2025-02-08 10:54:06 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,22 @@
 #include "../header/main.hpp"
+#include <cstring>
 #include <iostream>
-
 
 int main() {
     /* code */
+
+    constexpr auto str{"I am a C/C++ Developer ..."};
+    constexpr auto target{'a'};
+    const char *result{str};
+    size_t counter{};
+
+    while ((result = std::strchr(result, target)) != nullptr) {
+        std::cout << result << NL;
+        ++counter;
+        ++result;
+    }
+
+    std::cout << " I found " << counter << " of target." << NL;
 
     std::cout << "\n #(04:52:37): The End ..." << std::endl;
     return EXIT_SUCCESS;
