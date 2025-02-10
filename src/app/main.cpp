d4e8412b87504ed1c98caab8b24d62c26c0503a0
hinata <hinatachikao1971@gmail.com>
2025-02-10 18:13:51 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,25 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
-#include <string>
+#include "../header/main.hpp"
+
 
 int main() {
 
-    std::string full_name{};
-    std::string planet{"Earth, where the sky is blue"};
-    std::string preferred_planet{planet};
 
-    std::string message{"Hello there", 7}; /* print seven first character */
-    std::string weird_message{48, 'e'};
-    std::string greeting{"Hello World"};
-    // std::string say_hello{greeting, 6, 5};
 
-    std::cout << "full name is: " << full_name << full_name << NL;
-    std::cout << "planet is: " << planet << NL;
-    std::cout << "preferred planet is: " << preferred_planet << NL;
-    std::cout << "message is: " << message << NL;
-    std::cout << "weird message: " << weird_message << NL;
-    std::cout << "greeting is: " << greeting << NL;
-
-    std::cout << "\n #(21:36:50): The End ..." << std::endl;
+    std::cout << "\n #(18:13:51): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
