4b35ed791c5b296555df0880bccdfe5cf43298b0
hinata <hinatachikao1971@gmail.com>
2025-03-01 16:23:49 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,7 +3,15 @@
 
 int main() {
 
-        
+    const std::string str1{"Hello"};
+    const std::string str2{"World!"};
+    char space{'\x20'};                 /* Ok */
+    // const char space{'\x20'};        /* Ok */
+    // const char *space{" "};          /* Ok */
+    // const char *const space{" "};    /* Ok */
+    const std::string concat{str1 + space + str2};
+
+    std::cout << "concatenate result is: " << concat << NL;
 
 
     std::cout << "\n #(21:57:45): The End ..." << std::endl;
