8dceda145071c2045f043f79e6e6dfdcdf6d121f
hinata <hinatachikao1971@gmail.com>
2025-03-01 16:23:49 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,19 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
+
 
 int main() {
 
-    const std::string str1{"Hello"};
-    const std::string str2{"World!"};
-    char space{'\x20'};                 /* Ok */
-    // const char space{'\x20'};        /* Ok */
-    // const char *space{" "};          /* Ok */
-    // const char *const space{" "};    /* Ok */
-    const std::string concat{str1 + space + str2};
-
-    std::cout << "concatenate result is: " << concat << NL;
 
 
-    std::cout << "\n #(21:57:45): The End ..." << std::endl;
+    std::cout << "\n #(16:23:49): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
