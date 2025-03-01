b6f4b701189301e1ee83e6abc52710f330531376
hinata <hinatachikao1971@gmail.com>
2025-03-01 19:33:01 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    const std::string str1{"Hello"};
+    const std::string str2{"World"};
+    std::string str3{};
+    str3.append(str1);
+    str3.append(" ");
+    str3.append(str2);
+
+    std::cout << " str3: " << str3 << NL;
 
 
     std::cout << "\n #(18:11:13): The End ..." << std::endl;
