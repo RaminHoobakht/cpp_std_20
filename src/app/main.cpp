cfe3889520a9ae0d368bc1b0d6980273d0137998
hinata <hinatachikao1971@gmail.com>
2025-03-02 02:27:54 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,7 +4,12 @@
 
 int main() {
 
+    const std::string message{"I am a C/C++ Developer ..."};
 
+    for( const char item : message) {
+        std::cout << item << '\x20';
+    }
+    std::cout << '\n';
 
     std::cout << "\n #(21:59:22): The End ..." << std::endl;
     return EXIT_SUCCESS;
