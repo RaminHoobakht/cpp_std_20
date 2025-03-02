65f6fc7a9d26cca04ef82b562ff37f0b51a23cd3
hinata <hinatachikao1971@gmail.com>
2025-03-02 14:32:48 +0330

    new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,28 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
-    /* Code */
 
-    std::string str1{"Hello"};
-    std::string str2{"Hello"};
 
-    str1 += "67";
-    std::cout << "str1: " << str1 << NL;
-    util::separator();
 
-    str1 += 67;
-    std::cout << "str1: " << str1 << NL;
-    util::separator();
-
-    str2 += std::to_string(67);
-    std::cout << "str2: " << str2 << NL;
-    util::separator();
-
-    std::cout << "Hello" + std::to_string(23.369f) << NL;
-    util::separator();
-
-    std::cout << "\n #(21:59:22): The End ..." << std::endl;
+    std::cout << "\n #(14:32:48): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
