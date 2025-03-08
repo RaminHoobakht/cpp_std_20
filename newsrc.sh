73e2d62817c93be4bb6a319ac7e2542341e3de01
hinata <hinatachikao1971@gmail.com>
2025-03-09 00:10:45 +0330

    before creating new source file

--- newsrc.sh
+++ newsrc.sh
@@ -3,14 +3,14 @@
 clear
 ls -ls archive/src_*
 echo "enter file number: "
-read -r fileno
+read -r file_no
 
 git add --all
 git commit -m 'before creating new source file'
 
-mv src/ archive/src_"$fileno"
-cp CMakeLists.txt archive/src_"$fileno"/
-cp cmake_defaulte.txt CMakeLists.txt
+mv src/ archive/src_"$file_no"
+cp CMakeLists.txt archive/src_"$file_no"/
+cp cmake_default.txt CMakeLists.txt
 
 mkdir src
 mkdir src/app
@@ -38,28 +38,49 @@ printf '%s\n' "#ifndef HEADER_MAIN_CPP
 #define NL          '\n'
 #define RT          '\r'
 #define SP          '\x20'
-#define LINE_FEED   std::cout << NL
+#define LINEFEED   std::cout << NL
 #define SEP         util::separator()
 
 namespace util {
-
     constexpr size_t zero{0LU};
 
     void separator(size_t, char) noexcept;
 
 
-    inline void separator(const size_t no = 64, const char ch = '-') noexcept {
+    inline void separator(const size_t no = 64LU, const char ch = '-') noexcept {
         for (size_t i{zero}; i < no; ++i) {
             std::cout << ch;
         }
         std::cout << NL;
     }
 
+    class fs_exception final : public std::exception {
+    private:
+        std::string message_{};
 
+    public:
+        explicit fs_exception(std::string message)
+            : message_(std::move(message)) {
+        }
+
+        fs_exception(const exception &exception, std::string message)
+            : std::exception(exception),
+              message_(std::move(message)) {
+        }
+
+        fs_exception(exception &&exception, std::string message)
+            : std::exception(exception),
+              message_(std::move(message)) {
+        }
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return this->message_.c_str();
+        }
+    };
 } // namespace util
 
 #endif /* HEADER_MAIN_CPP */" >> src/header/main.hpp
 
 git add --all
-git commit -m 'new source file'
+git commit -m 'after creating new source file'
 
--- src/header/main.hpp
+++ src/header/main.hpp
@@ -6,24 +6,45 @@
 #define NL          '\n'
 #define RT          '\r'
 #define SP          '\x20'
-#define LINE_FEED   std::cout << NL
+#define LINEFEED   std::cout << NL
 #define SEP         util::separator()
 
 namespace util {
-
     constexpr size_t zero{0LU};
 
     void separator(size_t, char) noexcept;
 
 
-    inline void separator(const size_t no = 64, const char ch = '-') noexcept {
+    inline void separator(const size_t no = 64LU, const char ch = '-') noexcept {
         for (size_t i{zero}; i < no; ++i) {
             std::cout << ch;
         }
         std::cout << NL;
     }
 
+    class fs_exception final : public std::exception {
+    private:
+        std::string message_{};
 
+    public:
+        explicit fs_exception(std::string message)
+            : message_(std::move(message)) {
+        }
+
+        fs_exception(const exception &exception, std::string message)
+            : std::exception(exception),
+              message_(std::move(message)) {
+        }
+
+        fs_exception(exception &&exception, std::string message)
+            : std::exception(exception),
+              message_(std::move(message)) {
+        }
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return this->message_.c_str();
+        }
+    };
 } // namespace util
 
 #endif /* HEADER_MAIN_CPP */
