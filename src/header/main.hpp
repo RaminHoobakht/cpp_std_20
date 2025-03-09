853757e3369aa998f3cc79f564160e4b89ba839f
hinata <hinatachikao1971@gmail.com>
2025-03-09 22:16:04 +0330

    before creating new source file

--- src/header/main.hpp
+++ src/header/main.hpp
@@ -3,11 +3,11 @@
 
 #include <iostream>
 
-#define NL          '\n'
-#define RT          '\r'
-#define SP          '\x20'
-#define LINEFEED   std::cout << NL
-#define SEP         util::separator()
+#define NL '\n'
+#define RT '\r'
+#define SP '\x20'
+#define LINEFEED std::cout << NL
+#define SEP util::separator()
 
 namespace util {
     constexpr size_t zero{0LU};
@@ -15,31 +15,29 @@ namespace util {
     void separator(size_t, char) noexcept;
 
 
-    inline void separator(const size_t no = 64LU, const char ch = '-') noexcept {
+    inline void separator(const size_t no = 64LU,
+                          const char ch = '-') noexcept {
         for (size_t i{zero}; i < no; ++i) {
             std::cout << ch;
         }
         std::cout << NL;
     }
 
-    class fs_exception final : public std::exception {
+    /* -------------------------------------------------- */
+
+    class publicexception final : public std::exception {
     private:
         std::string message_{};
 
     public:
-        explicit fs_exception(std::string message)
-            : message_(std::move(message)) {
-        }
+        explicit publicexception(std::string message) :
+            message_(std::move(message)) {}
 
-        fs_exception(const exception &exception, std::string message)
-            : std::exception(exception),
-              message_(std::move(message)) {
-        }
+        // publicexception(const exception &exception, std::string message) :
+        //     std::exception(exception), message_(std::move(message)) {}
 
-        fs_exception(exception &&exception, std::string message)
-            : std::exception(exception),
-              message_(std::move(message)) {
-        }
+        // publicexception(exception &&exception, std::string message) :
+        //     std::exception(exception), message_(std::move(message)) {}
 
         [[nodiscard]] const char *what() const noexcept override {
             return this->message_.c_str();
