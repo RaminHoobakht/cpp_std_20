a400cd09d45f2687c3b8b5f9bfc365f911d30d77
hinata <hinatachikao1971@gmail.com>
2025-03-19 12:10:18 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,40 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
+
 
 int main() {
 
-    std::string name{"hinata chikao"}; // size = 13
-
-    std::cout << "value of name is: " << name << NL;
-    std::cout << "size of the name is: " << name.size() << NL;
-
-    const size_t size{name.size()};
-
-    LINEFEED;
-    LINEFEED;
-    std::cout << "print name value in for loop:" << NL;
-    for (size_t i{util::zero}; i < size; ++i) {
-        std::cout << name[i] << SP;
-    }
-
-    LINEFEED;
-    LINEFEED;
-    std::cout << "print name value in range base for loop: " << NL;
-    for (const char &item: name) {
-        std::cout << item << SP;
-    }
-
-    LINEFEED;
-    LINEFEED;
-    std::cout << "print name value in for loop by 'at' function: " << NL;
-    for (size_t i{util::zero}; i < size; ++i) {
-        std::cout << name.at(i) << SP;
-    }
-
-    LINEFEED;
-    LINEFEED;
 
 
-    std::cout << "\n #(19:02:32): The End ..." << std::endl;
+    std::cout << "\n #(12:10:18): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
