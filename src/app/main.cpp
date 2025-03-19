c31034d296c05e55d2373bcbe389b1947450b11a
hinata <hinatachikao1971@gmail.com>
2025-03-19 13:43:56 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    std::string name{"hinata chikao"};
 
+    std::cout << "vaule of name is   : " << name << NL;
+
+    const char *my_name{name.c_str()};
+
+    std::cout << "value of my_name is: " << my_name << NL;
+
+    /* ------------------------------ */
+    LINEFEED;
+    LINEFEED;
+
+    char *my_name2{name.data()};
+
+    std::cout << "before change-> value of my_name2 is: " << my_name2 << NL;
+
+    my_name2[3] = 'o';
+    my_name2[5] = 'o';
+
+    std::cout << "after change -> value fo my_name2 is: " << my_name2 << NL;
 
     std::cout << "\n #(12:42:11): The End ..." << std::endl;
     return EXIT_SUCCESS;
