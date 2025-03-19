3d0cdcf63a9dfa68755ad7d5d177d22b4f39647c
hinata <hinatachikao1971@gmail.com>
2025-03-19 13:56:34 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,32 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    std::string str1{"Hello World"};
-    std::string str2;
-    std::string str3{};
-
-    LINEFEED;
-    std::cout << " usnig empty functoin: " << NL;
-    std::cout << "\tstr1 is empty: " << std::boolalpha << str1.empty() << NL;
-    std::cout << "\tstr2 is empty: " << std::boolalpha << str2.empty() << NL;
-    std::cout << "\tstr3 is empty: " << std::boolalpha << str3.empty() << NL;
-
-    LINEFEED;
-    std::cout << " using size functoin: " << NL;
-    std::cout << "\tstr1 contains " << str1.size() << " character." << NL;
-    std::cout << "\tstr2 contains " << str2.size() << " character." << NL;
-    std::cout << "\tstr3 contains " << str3.size() << " character." << NL;
-
-    LINEFEED;
-    std::cout << " using length functoin: " << NL;
-    std::cout << "\tstr1 contains " << str1.length() << " character." << NL;
-    std::cout << "\tstr2 contains " << str2.length() << " character." << NL;
-    std::cout << "\tstr3 contains " << str3.length() << " character." << NL;
 
 
-    std::cout << "\n #(13:43:56): The End ..." << std::endl;
+    std::cout << "\n #(13:56:34): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
