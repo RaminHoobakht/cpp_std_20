ba2d44e047a297e110ffee4f3a7f1ef16b62d51c
hinata <hinatachikao1971@gmail.com>
2025-03-20 16:08:36 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,28 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    std::string name{"Hinata Chikao"};
+
+    std::cout << "before insert : " << name << NL;
+
+    name.insert(6, 1, ',');
+
+    std::cout << "after insert  : " << name << NL;
+
+    LINEFEED;
+    util::separator();
+
+    name.clear();
+    name = "Hinata Chikao";
+    const char *text{" is my friend."};
+
+    name.insert(13, text);
+
+    std::cout << "after insert a text at the end of name: " << NL;
+    std::cout << "result is: " << name << NL;
 
 
     std::cout << "\n #(15:44:00): The End ..." << std::endl;
