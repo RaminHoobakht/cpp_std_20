e5508026dd93ebd9e246369a9bf804c316e99c97
hinata <hinatachikao1971@gmail.com>
2025-03-20 19:22:45 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,34 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    std::string word{};
 
-    word.push_back('H');
-    word.push_back('e');
-    word.push_back('l');
-    word.push_back('l');
-    word.push_back('o');
 
-    std::cout << "value of world is: " << word << NL;
-
-    std::cout << "back character is : " << word.back() << NL;
-    std::cout << "front character is: " << word.front() << NL;
-
-    LINEFEED;
-    util::separator();
-
-    word.pop_back();
-    std::cout << "after pop back one  : " << word << NL;
-
-    word.pop_back();
-    std::cout << "after pop back two  : " << word << NL;
-
-    word.pop_back();
-    std::cout << "after pop back three: " << word << NL;
-
-    std::cout << "\n #(18:37:33): The End ..." << std::endl;
+    std::cout << "\n #(19:22:45): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
