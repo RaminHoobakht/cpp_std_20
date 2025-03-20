2023cdb3be634ddbb6b769926031a58a08c280af
hinata <hinatachikao1971@gmail.com>
2025-03-20 23:32:12 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,31 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    const std::string word_one{"Hello"};
-    const std::string word_two{"Iello"};
 
-    std::cout << std::boolalpha;
 
-    std::cout << "word_one is: " << word_one << NL;
-    std::cout << "word_two is: " << word_two << NL;
-    util::separator();
-
-    std::cout << "word_one == word_two: " << (word_one == word_two) << NL;
-    util::separator();
-
-    std::cout << "word_one > word_two : " << (word_one > word_two) << NL;
-    std::cout << "word_one < word_two : " << (word_one < word_two) << NL;
-    util::separator();
-
-    std::cout << "word_one >= word_two: " << (word_one >= word_two) << NL;
-    std::cout << "word_one <= word_two: " << (word_one <= word_two) << NL;
-    util::separator();
-
-    std::cout << "word_one != word_two: " << (word_one != word_two) << NL;
-
-    std::cout << "\n #(19:22:45): The End ..." << std::endl;
+    std::cout << "\n #(23:32:12): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
