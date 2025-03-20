119aff8286ff15fb988bbdb89725e85683224f3a
hinata <hinatachikao1971@gmail.com>
2025-03-20 18:37:33 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    std::string name{"12345678901234567890"};
 
+    std::cout << "before erase: " << name << NL;
+
+    name.erase(5, 5);
+
+    std::cout << "after erase : " << name << NL;
 
     std::cout << "\n #(17:40:26): The End ..." << std::endl;
     return EXIT_SUCCESS;
