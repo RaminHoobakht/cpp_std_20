05a1952212859afe525a27e8b3585505660145b2
hinata <hinatachikao1971@gmail.com>
2025-03-20 15:43:59 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,25 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
 
 
 int main() {
 
+    std::string name{"Hinata Chikao"};
 
+    std::cout << "size of name is     : " << name.size() << NL;
+    std::cout << "length of name is   : " << name.length() << NL;
+    std::cout << "capacity of name is : " << name.capacity() << NL;
+    std::cout << "max size of name is : " << name.max_size() << NL;
+
+    LINEFEED;
+    util::separator();
+
+    name.clear();
+
+    std::cout << "size of name is     : " << name.size() << NL;
+    std::cout << "length of name is   : " << name.length() << NL;
+    std::cout << "capacity of name is : " << name.capacity() << NL;
+    std::cout << "max size of name is : " << name.max_size() << NL;
 
     std::cout << "\n #(23:46:10): The End ..." << std::endl;
     return EXIT_SUCCESS;
