912cc436655213ce78d8abff0daf70da1c71ca4f
hinata <hinatachikao1971@gmail.com>
2025-03-21 22:14:30 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,27 +1,11 @@
-#include "../header/main.hpp"
-#include <ios>
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    const std::string word_one{"Hello"};
-    const char word_two[]{'B', 'e', 'l', 'l', 'o', '\0'};
 
-    std::cout << word_one << NL;
-    std::cout << word_two << NL;
 
-    LINEFEED;
-    util::separator();
-
-    std::cout << std::boolalpha;
-    std::cout << "word_one == word_two: " << (word_one == word_two) << NL;
-    std::cout << "word_one >  word_two: " << (word_one > word_two) << NL;
-    std::cout << "word_one <  word_two: " << (word_one < word_two) << NL;
-    std::cout << "word_one >= word_two: " << (word_one >= word_two) << NL;
-    std::cout << "word_one <= word_two: " << (word_one <= word_two) << NL;
-    std::cout << "word_one != word_two: " << (word_one != word_two) << NL;
-
-    std::cout << "\n #(20:38:57): The End ..." << std::endl;
+    std::cout << "\n #(22:14:30): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
