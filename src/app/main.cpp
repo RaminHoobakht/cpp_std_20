b585f03caf0d50ed2d4f8c576b1771019fa938d7
hinata <hinatachikao1971@gmail.com>
2025-03-21 06:35:12 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,21 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <ios>
+#include <iostream>
 
 
 int main() {
 
-	
+    const std::string word_one{"Hello"};
+    const char *word_two{"World"};
+
+    std::cout << std::boolalpha;
+    std::cout << "word_one == word_two: " << (word_one == word_two) << NL;
+    std::cout << "word_one > word_two : " << (word_one > word_two) << NL;
+    std::cout << "word_one < word_two : " << (word_one < word_two) << NL;
+    std::cout << "word_one >= word_two: " << (word_one >= word_two) << NL;
+    std::cout << "word_one <= word_two: " << (word_one <= word_two) << NL;
+    std::cout << "word_one != word_two: " << (word_one != word_two) << NL;
+
 
     std::cout << "\n #(23:32:12): The End ..." << std::endl;
     return EXIT_SUCCESS;
