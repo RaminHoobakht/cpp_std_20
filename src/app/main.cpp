3080c170faf6df8ded5a2b9d9893cefe3ffa0e77
hinata <hinatachikao1971@gmail.com>
2025-03-23 19:38:01 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,29 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
-#include <string>
+#include "../header/main.hpp"
+
 
 int main() {
 
-    std::string message{"Hello world"};
-    std::string word{"Iran"};
 
-    std::cout << "before replace: " << message << NL;
-    LINEFEED;
-    util::separator();
 
-    std::basic_string<char> &result{message.replace(6, 10, word)};
-
-    std::cout << "after replace : " << message << NL;
-    std::cout << "output of the replace function: " << result << NL;
-
-    LINEFEED;
-    util::separator();
-
-    message.insert(6, "... ");
-
-    std::cout << "after insert : " << message << NL;
-    std::cout << "output of the replace function: " << result << NL;
-
-    std::cout << "\n #(17:52:44): The End ..." << std::endl;
+    std::cout << "\n #(19:38:01): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
