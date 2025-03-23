95cf7cb820701b1880078bbe549136e613d14bd3
hinata <hinatachikao1971@gmail.com>
2025-03-23 17:10:19 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,27 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    std::string name_one{"Hinata"};
-    std::string name_two{"Hinata"};
-
-    const int result1{name_one.compare(0, 6, name_two)};
-
-    std::cout << "value result one is: " << result1 << NL;
-
-    name_one.clear();
-    name_two.clear();
-
-    name_one = "Renata";
-    name_two = "Tinata";
-
-    const int result2{name_one.compare(2, 4, name_two)};
-
-    std::cout << "value result two is: " << result2 << NL;
 
 
-    std::cout << "\n #(22:42:39): The End ..." << std::endl;
+    std::cout << "\n #(17:10:19): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
