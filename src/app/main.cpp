498f4b546ccf58d32738abc4ed47f3745d90066c
hinata <hinatachikao1971@gmail.com>
2025-03-24 12:32:55 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,20 +1,11 @@
-/* using replace function in string class */
-
-#include "../header/main.hpp"
 #include <iostream>
+#include "../header/main.hpp"
 
 
 int main() {
 
-    std::string str1{"I like to be a Python Developer ..."};
-    std::string str2{"I am a C/C++ Developer ..."};
 
-    std::cout << "str1 before replace: " << str1 << NL;
 
-    str1.replace(15, 20, str2, 7, 11);
-
-    std::cout << "str1 after  replace: " << str1 << NL;
-
-    std::cout << "\n #(02:22:34): The End ..." << std::endl;
+    std::cout << "\n #(12:32:55): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
