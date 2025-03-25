aec06034969750f75186cbac7be441cad03ec72d
hinata <hinatachikao1971@gmail.com>
2025-03-25 16:19:25 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -6,6 +6,29 @@
 
 int main() {
 
+    LINEFEED;
+
+    std::string message{"Hello World"};
+    std::string message_copy{message};
+
+    std::cout << "message     : " << message << NL;
+    std::cout << "message_copy: " << message_copy << NL;
+    util::separator();
+
+    char *p1{message.data()};
+    char *p2{message_copy.data()};
+
+    std::cout << "(void *)p1  : " << reinterpret_cast<void *>(p1) << NL;
+    std::cout << "(void *)p2  : " << reinterpret_cast<void *>(p2) << NL;
+    util::separator();
+
+    std::cout << "*p1 : " << *p1 << NL;
+    std::cout << "*p2 : " << *p2 << NL;
+    util::separator();
+
+    std::cout << "p1 : " << p1 << NL;
+    std::cout << "p2 : " << p2 << NL;
+    util::separator();
 
     std::cout << "\n #(15:29:48): The End ..." << std::endl;
     return EXIT_SUCCESS;
