230536a48ba8c6dd906d37fd0ab1c0a9aec1eee4
hinata <hinatachikao1971@gmail.com>
2025-03-25 12:44:56 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,26 @@
-/*  */
+/* using functions: stoul, stoull */
 
 #include "../header/main.hpp"
 #include <iostream>
+#include <string>
 
 
 int main() {
+
+    LINEFEED;
+    util::separator();
+
+    const std::string str_number{"34.567"};
+
+    unsigned long int uli_var{std::stoul(str_number)};
+    std::cout << "uli_var: " << uli_var << NL;
+    std::cout << "size of uli_var: " << sizeof(uli_var) << NL;
+    util::separator();
+
+    unsigned long long int ulli_var{std::stoul(str_number)};
+    std::cout << "ulli_var: " << ulli_var << NL;
+    std::cout << "size of ulli_var: " << sizeof(ulli_var) << NL;
+    util::separator();
 
 
     std::cout << "\n #(12:33:17): The End ..." << std::endl;
