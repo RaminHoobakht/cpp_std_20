f7c90c68f5bca684192611c65613569a5736fdbf
hinata <hinatachikao1971@gmail.com>
2025-03-25 17:14:42 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -6,16 +6,7 @@
 
 int main() {
 
-    LINEFEED;
 
-    const std::string message1{"Hello"};
-    const std::string message2{message1};
-    const std::string message3{message1};
-
-    std::cout << "address of message1: " << &message1 << NL;
-    std::cout << "address of message2: " << &message2 << NL;
-    std::cout << "address of message3: " << &message3 << NL;
-
-    std::cout << "\n #(16:19:25): The End ..." << std::endl;
+    std::cout << "\n #(17:14:42): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
