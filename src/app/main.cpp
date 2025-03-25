675e705448dfae87490c05a832d7c6cf37316017
hinata <hinatachikao1971@gmail.com>
2025-03-25 14:17:18 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -6,6 +6,31 @@
 
 int main() {
 
+    LINEFEED;
+
+    const std::string message1{R"(
+			this is line one.
+		this is line two.
+	this is line three.
+	)"};
+
+    std::cout << message1 << NL;
+
+    std::string message2{};
+
+    message2 = R"(
+		I am
+		a
+		C
+		and 
+		C++
+		Developer.
+	)";
+
+    util::separator();
+    LINEFEED;
+
+    std::cout << message2 << NL;
 
     std::cout << "\n #(13:43:07): The End ..." << std::endl;
     return EXIT_SUCCESS;
