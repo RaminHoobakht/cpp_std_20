4e71eb61d14b2c7016dc30208a5017ccade1a30d
hinata <hinatachikao1971@gmail.com>
2025-03-26 21:34:41 +0330

    before creating new source file

---
+++ src/header/compare.cpp
@@ -0,0 +1,13 @@
+
+#include "compare.hpp"
+
+
+/* max function implementation */
+int max(int a, int b) noexcept {
+
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
---
+++ src/header/compare.hpp
@@ -0,0 +1,8 @@
+#ifndef SRC_HEADER_COMPARE_HPP_
+#define SRC_HEADER_COMPARE_HPP_
+
+
+int max(int a, int b) noexcept;
+
+
+#endif /* SRC_HEADER_COMPARE_HPP_ */
