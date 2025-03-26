9fe6f9cc3b230b4ff9f4cec226302eab7bb88c51
hinata <hinatachikao1971@gmail.com>
2025-03-26 12:23:51 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,4 +1,4 @@
-/* using string_view */
+/*  */
 
 #include "../header/main.hpp"
 #include <iostream>
@@ -6,24 +6,7 @@
 
 int main() {
 
-    std::string_view sv1{};
 
-    {
-        std::string message{"an apple a day, keeps the doctor away."};
-        sv1 = message;
-
-        std::cout << "(within block) message: " << message << NL;
-        std::cout << "(within block) sv1    : " << sv1 << NL;
-        util::separator();
-    }
-
-    /* here is out of block and the message variable was removed from memory */
-    // std::cout << "(out of block) message: " << message << NL;
-
-    /* because hear is out of block, content of the sv1 was erased */
-    std::cout << "(out of block) sv1    : " << sv1 << NL;
-
-
-    std::cout << "\n #(12:00:47): The End ..." << std::endl;
+    std::cout << "\n #(12:23:51): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
