8ceaf23a451886590bf06dedb1581ffd5986893b
hinata <hinatachikao1971@gmail.com>
2025-03-26 15:02:04 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,21 @@
-/*  */
+/* viewing non null terminated string (BAD) */
 
 #include "../header/main.hpp"
+#include <cstring>
 #include <iostream>
 
 
 int main() {
 
+    char char_array[]{'H', 'l', 'l', 'o'};
+    std::string_view sv{char_array};
+
+
+    /* encounter with compile error */
+    std::cout << "sv has " << std::strlen(sv.data()) << sv << NL;
+
+    /* encounter with compile error */
+    std::cout << "sv.data(): " << sv.data() << NL;
 
     std::cout << "\n #(13:27:45): The End ..." << std::endl;
     return EXIT_SUCCESS;
