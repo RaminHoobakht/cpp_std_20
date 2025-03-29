51f6c4361ace85d30f3f75a3ad4d1143895f4117
hinata <hinatachikao1971@gmail.com>
2025-03-29 19:00:48 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,34 +1,12 @@
-/* sized array passed by reference */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
-
-signed long long int
-sum_array(__restrict_arr const int (&scores)[10]) noexcept(true);
+#include <iostream>
 
 
 int main() {
 
-    int arr[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    signed long long int result{sum_array(arr)};
-    std::cout << "sum of array number is: " << result << NL;
-
-
-    std::cout << "\n #(16:23:36): The End ..." << std::endl;
+    std::cout << "\n #(19:00:48): The End ..." << std::endl;
     return EXIT_SUCCESS;
-}
-
-/* ------------------------------------ */
-
-signed long long int
-sum_array(__restrict_arr const int (&scores)[10]) noexcept(true) {
-
-    signed long long int sum{};
-    const size_t size{std::size(scores)};
-    for (size_t i{util::zero}; i < size; ++i) {
-        sum += scores[i];
-    }
-
-    return sum;
 }
