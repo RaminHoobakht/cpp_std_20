7a0147c0bd6ac3d5fa9ae33a78070f37ebd4c196
hinata <hinatachikao1971@gmail.com>
2025-03-29 22:11:41 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,33 +1,12 @@
-/* passing sized array by reference  */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
-typedef signed long long int lli;
-
-lli sum_array(__restrict_arr const int (&scores)[10]) noexcept(true);
 
 int main() {
 
-    int int_arr[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    lli sum{sum_array(int_arr)};
 
-    std::cout << "sum of array is: " << sum << NL;
-
-    std::cout << "\n #(19:00:48): The End ..." << std::endl;
+    std::cout << "\n #(22:11:41): The End ..." << std::endl;
     return EXIT_SUCCESS;
-}
-
-/* ------------------------ */
-
-lli sum_array(__restrict_arr const int (&scores)[10]) noexcept(true) {
-
-    lli sum{};
-    
-    /* size of array is sent. so it can using range base for loop */
-    for (const int &item: scores) {
-        sum += item;
-    }
-
-    return sum;
 }
