93ae986dfad179c5005189ec9ea61439f20ddbbc
hinata <hinatachikao1971@gmail.com>
2025-03-31 03:04:51 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,22 @@
-/*  */
+/* using consteval */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
+
+consteval int get_value(int value) noexcept {
+    /* code */
+    return 3 * value;
+}
 
 
 int main() {
+
+
+    int result{get_value(50)};
+    std::cout << "result is: " << result << NL;
+
+    int number{70};
+    // int result2{get_value(number)}; /* compile time error */
 
 
     std::cout << "\n #(02:57:21): The End ..." << std::endl;
