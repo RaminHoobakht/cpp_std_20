52be807009caa86ee3792add9357518f6358b8ea
hinata <hinatachikao1971@gmail.com>
2025-03-31 02:57:21 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,25 +1,12 @@
 /*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
-constexpr int get_value(int value) noexcept;
 
 int main() {
 
-    // constexpr int result = get_value(10);
-    // std::cout << result << NL;
 
-    std::cout << "result is: " << get_value(5) << NL;
-
-    int a{54};
-    std::cout << "result is: " << get_value(a) << NL;
-
-    std::cout << "\n #(02:35:55): The End ..." << std::endl;
+    std::cout << "\n #(02:57:21): The End ..." << std::endl;
     return EXIT_SUCCESS;
-}
-
-constexpr int get_value(int value) noexcept {
-    /* code */
-    return 3 * value;
 }
