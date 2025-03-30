2be0a6f5cb37800199339f4a5fda4e4c7359e38d
hinata <hinatachikao1971@gmail.com>
2025-03-30 20:51:43 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,21 @@
-/*  */
+/* implicit conversoin */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
+void print_out(int &num) noexcept(true);
 
 int main() {
 
+    double number{12036};
+
+    print_out(number);
 
     std::cout << "\n #(20:31:24): The End ..." << std::endl;
     return EXIT_SUCCESS;
+}
+
+void print_out(int &num) noexcept(true) {
+    ++num;
+    std::cout << "value on input number is: " << num << NL;
 }
