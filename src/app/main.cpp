f4561cde9ffada67838b6810daab382910ae76fc
hinata <hinatachikao1971@gmail.com>
2025-03-30 20:31:24 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,22 @@
-/*  */
+/* default function arguments */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
+void compute(int age = 54, double weight = 70.5,
+             double distance = 4) noexcept(true);
 
 int main() {
 
+    compute(30);
 
     std::cout << "\n #(11:37:33): The End ..." << std::endl;
     return EXIT_SUCCESS;
+}
+
+void compute(const int age, const double weight,
+             const double distance) noexcept(true) {
+    std::cout << "Doing computing on age     : " << age << NL;
+    std::cout << "Doing computing on weight  : " << weight << NL;
+    std::cout << "Doing computing on distance: " << distance << NL;
 }
