614af81f1bbff07e11d64392654063622e62c478
hinata <hinatachikao1971@gmail.com>
2025-03-31 02:17:17 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,22 @@
-/*  */
+/* passign string to the functoin with std::string_view (Good) */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
+void say_my_name(std::string_view name) noexcept;
 
 int main() {
 
+    std::string my_name{"Hinatai Chikao"};
+
+    say_my_name(my_name);
+    say_my_name("Daniel");
+    say_my_name(std::string_view("Raphael"));
 
     std::cout << "\n #(21:33:42): The End ..." << std::endl;
     return EXIT_SUCCESS;
+}
+
+void say_my_name(std::string_view name) noexcept {
+    std::cout << "Your name is: " << name << NL;
 }
