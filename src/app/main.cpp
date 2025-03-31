72653d83452cca4d8cf9285c691211d826f45522
hinata <hinatachikao1971@gmail.com>
2025-03-31 14:58:05 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,33 +1,12 @@
-/* using enum class */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 
 int main() {
 
-    enum class month {
-        Jan = 1,
-        Feb,
-        Mar,
-        Apr,
-        May,
-        Jun,
-        Jul,
-        Agu = 18,
-        Sep,
-        Oct,
-        Nov,
-        Dec
-    };
 
-    month my_month{month::Jan};
-    std::cout << "my_month is: " << static_cast<int>(my_month) << NL;
-
-    std::cout << "value of Agu: " << static_cast<int>(month::Agu) << NL;
-    std::cout << "value of Sep: " << static_cast<int>(month::Sep) << NL;
-
-
-    std::cout << "\n #(03:04:51): The End ..." << std::endl;
+    std::cout << "\n #(14:58:05): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
