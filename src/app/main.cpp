4313bc05cba08bd12ac4e5b05c272fa290789667
hinata <hinatachikao1971@gmail.com>
2025-03-31 15:27:49 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,94 +1,12 @@
 /*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 
 int main() {
 
-    enum class Month {
-        Jan = 1,
-        January = Jan,
-        Feb,
-        February = Feb,
-        Mar,
-        March = Mar,
-        Apr = 4,
-        April = Apr,
-        May = 5,
-        Jun = 6,
-        June = Jun,
-        Jul = 7,
-        July = Jul,
-        Aug = 8,
-        August = Aug,
-        Sep = 9,
-        September = Sep,
-        Oct = 10,
-        October = Oct,
-        Nov = 11,
-        November = Nov,
-        Dec = 12,
-        December = Dec
-    };
 
-    enum class Month1 : int {
-        Jan = 1,
-        January = Jan,
-        Feb,
-        February = Feb,
-        Mar,
-        March = Mar,
-        Apr = 4,
-        April = Apr,
-        May = 5,
-        Jun = 6,
-        June = Jun,
-        Jul = 7,
-        July = Jul,
-        Aug = 8,
-        August = Aug,
-        Sep = 9,
-        September = Sep,
-        Oct = 10,
-        October = Oct,
-        Nov = 11,
-        November = Nov,
-        Dec = 12,
-        December = Dec
-    };
-
-
-    enum class Month2 : unsigned char {
-        Jan = 1,
-        January = Jan,
-        Feb,
-        February = Feb,
-        Mar,
-        March = Mar,
-        Apr = 4,
-        April = Apr,
-        May = 5,
-        Jun = 6,
-        June = Jun,
-        Jul = 7,
-        July = Jul,
-        Aug = 8,
-        August = Aug,
-        Sep = 9,
-        September = Sep,
-        Oct = 10,
-        October = Oct,
-        Nov = 11,
-        November = Nov,
-        Dec = 12,
-        December = Dec
-    };
-
-    Month month{Month::August};
-    std::cout << "value of August is: " << static_cast<int>(month) << NL;
-
-
-    std::cout << "\n #(15:07:35): The End ..." << std::endl;
+    std::cout << "\n #(15:27:49): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
