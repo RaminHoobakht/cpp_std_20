17f36bf3315f159f2967a80cf2b387667bef0ebd
hinata <hinatachikao1971@gmail.com>
2025-03-31 19:23:47 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,15 @@
-/*  */
+/* passing parameter to the main function */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 
-int main() {
+int main(const int argc, const char **argv) {
 
+    std::cout << "The number of the parameter is: " << argc << NL;
+
+    std::cout << "index  notation-> parameter is: " << argv[0] << NL;
+    std::cout << "offset notation-> parameter is: " << *(argv + 0) << NL;
 
     std::cout << "\n #(17:03:33): The End ..." << std::endl;
     return EXIT_SUCCESS;
