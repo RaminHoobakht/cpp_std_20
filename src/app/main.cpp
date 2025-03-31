6ef14fa991f3fbd9e8ad246885157a6a2404cc1f
hinata <hinatachikao1971@gmail.com>
2025-03-31 19:33:12 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,20 @@
-/*  */
+/* passing parameter to the main function */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 
-int main() {
+int main(const int argc, const char **argv) {
 
+    LINEFEED;
+    std::cout << "Number of parameters is: " << argc << NL;
+    util::separator();
+
+    std::cout << "Parameter list: " << NL;
+    for (size_t i{util::zero}; i < static_cast<size_t>(argc); ++i) {
+        std::cout << *(argv + i) << NL;
+    }
+    LINEFEED;
 
     std::cout << "\n #(19:23:47): The End ..." << std::endl;
     return EXIT_SUCCESS;
