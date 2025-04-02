1a6751e6ad9ee6cd5ec958ec064fc7ccc1fb138f
hinata <hinatachikao1971@gmail.com>
2025-04-02 15:02:45 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,35 +1,12 @@
 /*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
-
-int &sum(int &a, int &b) noexcept;
 
 int main() {
 
-    LINEFEED;
 
-    int x{120};
-    int y{220};
-
-    int main_result{sum(x, y)};
-    std::cout << "result is: " << main_result << NL;
-    std::cout << "value of x is: " << x << NL;
-    std::cout << "value of y is: " << y << NL;
-    util::separator();
-
-    main_result++;
-    std::cout << "result is: " << main_result << NL;
-    std::cout << "value of x is: " << x << NL;
-    std::cout << "value of y is: " << y << NL;
-
-
-    std::cout << "\n #(11:24:35): The End ..." << std::endl;
+    std::cout << "\n #(15:02:45): The End ..." << std::endl;
     return EXIT_SUCCESS;
-}
-
-int &sum(int &a, int &b) noexcept {
-    a += b;
-    return a;
 }
