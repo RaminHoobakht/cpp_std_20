92fc55f63c3e0ff84bf0e7bf66ea5890518d502c
hinata <hinatachikao1971@gmail.com>
2025-04-02 15:21:27 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,34 @@
-/*  */
+/* getting data out of function */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
+// int &sum1(int &a, int &b) noexcept;
+int &sum2(int &a, int &b) noexcept;
 
 int main() {
 
+    int x{120};
+    int y{240};
+
+    int main_result1{sum2(x, y)++};
+    std::cout << "value main_result is: " << main_result1 << NL;
+    util::separator();
+
+    int main_result2{sum2(x, y)};
+    std::cout << "value main_result is: " << main_result2 << NL;
+    util::separator();
 
     std::cout << "\n #(15:02:45): The End ..." << std::endl;
     return EXIT_SUCCESS;
+}
+
+// int &sum1(int &a, int &b) noexcept {
+//     int local_result{a + b};
+//     return static_cast<int &>(local_result);
+// }
+
+int &sum2(int &a, int &b) noexcept {
+    static int local_result{a + b};
+    return static_cast<int &>(local_result);
 }
