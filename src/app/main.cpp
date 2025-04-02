2dd5e43837d8ce35689673ec5e9e61325aee4654
hinata <hinatachikao1971@gmail.com>
2025-04-02 16:45:40 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,45 +1,12 @@
-/* getting data out of function */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
-int &sum1(int &a, int &b) noexcept;
-const int &sum2(int &a, int &b) noexcept;
-const int &sum3(const int &a, const int &b) noexcept;
 
 int main() {
 
-    int x{120};
-    int y{220};
 
-    int main_result1{sum1(x, y)};
-    std::cout << "value of main_result1 is: " << main_result1 << NL;
-    util::separator();
-
-    int main_result2{sum2(x, y)};
-    std::cout << "value of main_result2 is: " << main_result2 << NL;
-    util::separator();
-
-    int main_result3{sum3(x, y)};
-    std::cout << "value of main_result3 is: " << main_result3 << NL;
-    util::separator();
-
-
-    std::cout << "\n #(15:21:27): The End ..." << std::endl;
+    std::cout << "\n #(16:45:40): The End ..." << std::endl;
     return EXIT_SUCCESS;
-}
-
-int &sum1(int &a, int &b) noexcept {
-    static int local_result{a + b};
-    return local_result;
-}
-
-const int &sum2(int &a, int &b) noexcept {
-    static int local_result{a + b};
-    return local_result;
-}
-
-const int &sum3(const int &a, const int &b) noexcept {
-    static int local_result{a + b};
-    return local_result;
 }
