6c9281df44266cdb0338636f1dc0b6e50d2ab812
hinata <hinatachikao1971@gmail.com>
2025-04-05 12:00:28 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,50 +1,12 @@
-/* return by reference within a function */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
-int &max(int &a, int &b) noexcept;
 
 int main() {
 
-    int x{120};
-    int y{220};
 
-    int &max_ref{max(x, y)};
-    int result{max(x, y)};
-    util::separator();
-
-    std::cout << "value of x is: " << x << NL;
-    std::cout << "value of y is: " << y << NL;
-
-    std::cout << "value of max_ref is: " << max_ref << NL;
-    std::cout << "value of result is : " << result << NL;
-    util::separator();
-
-    /* increments y variable. because it is greater then x */
-    ++max_ref;
-    std::cout << "value of x is: " << x << NL;
-    std::cout << "value of y is: " << y << NL;
-    util::separator();
-
-    /* increments y variable. because it is greater then x */
-    ++max_ref;
-    std::cout << "value of x is: " << x << NL;
-    std::cout << "value of y is: " << y << NL;
-    util::separator();
-
-    /* result variable is a fix variable because it is copy of the max function output. */
-    std::cout << "value of result is: " << result << NL;
-
-    std::cout << "\n #(09:54:00): The End ..." << std::endl;
+    std::cout << "\n #(12:00:28): The End ..." << std::endl;
     return EXIT_SUCCESS;
-}
-
-int &max(int &a, int &b) noexcept {
-
-    if (a > b) {
-        return a;
-    } else {
-        return b;
-    }
 }
