22de1fed7fc8e962a4160a0cf5683f458830e59b
hinata <hinatachikao1971@gmail.com>
2025-04-05 17:19:18 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,52 +1,12 @@
-/* returning array within function */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
-const double *find_maximum(const double scores[], const size_t size) noexcept;
-void display_array(const double arr[], const size_t size) noexcept;
 
 int main() {
 
-    double numbers[]{10.36,   25.23,        66.22,         85.24, 14.89,
-                     96.23,   102.325,      82.25,         69.36, 998.336,
-                     874.259, 1024.9558899, 1023.369852147};
 
-    /* ------------- */
-
-
-    size_t array_size{std::size(numbers)};
-    display_array(numbers, array_size);
-    const double *result{find_maximum(numbers, array_size)};
-    std::cout << "value of result is: " << *result << NL;
-    util::separator();
-
-    std::cout << "\n #(14:46:02): The End ..." << std::endl;
+    std::cout << "\n #(17:19:17): The End ..." << std::endl;
     return EXIT_SUCCESS;
-}
-
-/* ------------------------------- */
-
-const double *find_maximum(const double scores[], const size_t size) noexcept {
-
-    double max{0.0};
-    size_t index{util::zero};
-
-    for (size_t i{util::zero}; i < size; ++i) {
-        if (scores[i] > max) {
-            max = scores[i];
-            index = i;
-        }
-    }
-
-    return &scores[index];
-}
-
-/* ------------------------------- */
-
-void display_array(const double arr[], const size_t size) noexcept {
-    for (size_t i{util::zero}; i < size; ++i) {
-        std::cout << *(arr + i) << SP;
-    }
-    LINEFEED;
 }
