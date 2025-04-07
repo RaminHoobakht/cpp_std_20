60677ec8bdfd8b87612b5f76f7a71f897ba2fedd
hinata <hinatachikao1971@gmail.com>
2025-04-07 13:36:09 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,16 +1,26 @@
 /*  */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 #define out std::cout
 #define log std::clog
 #define err std::cerr
 
+int &max(int a, int b) noexcept;
 
 int main() {
 
 
     std::cout << "\n #(06:35:03): The End ..." << std::endl;
     return EXIT_SUCCESS;
+}
+
+/* encounter with error. because it referece to local variable. */
+int &max(int a, int b) noexcept {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
 }
