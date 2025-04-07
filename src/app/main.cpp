4eb46ed5a7f9297392c439bf0988e542ca13db9a
hinata <hinatachikao1971@gmail.com>
2025-04-07 06:35:03 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,16 +1,28 @@
 /*  */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 #define out std::cout
 #define log std::clog
 #define err std::cerr
 
+int &sum(int &a, int &b) noexcept;
 
 int main() {
 
+    int a{120};
+    int b{320};
+
+    auto result{sum(a, b)};
+    out << "value of result is: " << result << NL;
 
     std::cout << "\n #(05:42:59): The End ..." << std::endl;
     return EXIT_SUCCESS;
+}
+
+/* encounter with error. because return is referenced to thel local variable */
+int &sum(int &a, int &b) noexcept {
+    int local_resutl{a + b};
+    return local_resutl;
 }
