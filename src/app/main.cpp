ab009fb6579e25e2c94f77f31b31dbdacba2a57e
hinata <hinatachikao1971@gmail.com>
2025-04-09 23:36:39 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,32 +1,16 @@
 /*  */
 
-#include <iostream>
-#include <typeinfo>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 #define out std::cout
 #define log std::clog
 #define err std::cerr
 
-auto process_number(int value) noexcept;
 
 int main() {
 
-    int x{120};
 
-    /* encounter with compile time error */
-    auto result{process_number(x)};
-
-    std::cout << "\n #(19:00:52): The End ..." << std::endl;
-    return (EXIT_SUCCESS);
-}
-
-
-auto process_number(int value) noexcept {
-
-    if (value < 10) {
-        return (22);
-    } else {
-        return (33);
-    }
+    std::cout << "\n #(23:36:39): The End ..." << std::endl;
+    return EXIT_SUCCESS;
 }
