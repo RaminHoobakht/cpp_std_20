314affad27af31fc022c8160b4597e2af5ebe104
hinata <hinatachikao1971@gmail.com>
2025-04-09 17:14:57 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,7 @@
-/*  */
+/* constness preservation with deduced references */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 #define out std::cout
 #define log std::clog
@@ -10,6 +10,19 @@
 
 int main() {
 
+    const double some_other_var{44.33};
+    const double &const_ref{some_other_var};
+    
+    /* because p is bind to the const_ref and const_ref variable is const 
+     * so, p is const double */
+    auto &p{const_ref};
+
+    // ++p; /* enconuter with compile time error. because const_ref is const. */
+
+    out << "value of some_other_var is: " << some_other_var << NL;
+    out << "value of const_ref is     : " << const_ref << NL;
+    out << "value of p is             : " << p << NL;
+    SEP;
 
     std::cout << "\n #(14:36:16): The End ..." << std::endl;
     return EXIT_SUCCESS;
