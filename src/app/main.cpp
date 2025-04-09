1c9556f7b63430c16e918900f5f5e17ffb64b847
hinata <hinatachikao1971@gmail.com>
2025-04-09 19:00:52 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,7 @@
-/* non reference naked auto deduction: constness doesn't matter */
+/*  */
 
-#include <iostream>
-#include <typeinfo>
 #include "../header/utillib.hpp"
-
+#include <iostream>
 
 #define out std::cout
 #define log std::clog
@@ -12,24 +10,7 @@
 
 int main() {
 
-    const double const_variable{100.963};
-    auto x{const_variable};
 
-    out << "type of const_variable is: " << typeid(const_variable).name() << NL;
-    out << "type of x is             : " << typeid(x).name() << NL;
-    SEP;
-
-    out << "value of const_variable is : " << const_variable << NL;
-    out << "value of x is              : " << x << NL;
-    SEP;
-
-    ++x;
-
-    out << "after change value of x :" << NL;
-    out << "value of const_variable is : " << const_variable << NL;
-    out << "value of x is              : " << x << NL;
-    SEP;
-
-    std::cout << "\n #(17:14:57): The End ..." << std::endl;
+    std::cout << "\n #(19:00:52): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
