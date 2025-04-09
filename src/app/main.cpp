037e547bfe82ed1635bb4e44c2a53b7788a35a9a
hinata <hinatachikao1971@gmail.com>
2025-04-09 13:46:20 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,8 @@
-/*  */
+/* naked auto deduction */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include <typeinfo>
+#include "../header/utillib.hpp"
 
 #define out std::cout
 #define log std::clog
@@ -10,8 +11,20 @@
 
 int main() {
 
+    double some_var{55.5};
+    auto x{some_var};
+
+    out << "size of some_var is: " << sizeof(some_var) << NL;
+    out << "size of x is       : " << sizeof(x) << NL;
+    SEP;
+
+    out << "address of some_var is: " << &some_var << NL;
+    out << "address of x is       : " << &x << NL;
+    SEP;
+
+    out << "type of some_var is: " << typeid(some_var).name() << NL;
+    out << "type of x is       : " << typeid(x).name() << NL;
 
     std::cout << "\n #(15:59:26): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
-
