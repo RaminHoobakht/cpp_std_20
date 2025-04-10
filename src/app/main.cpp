da37f8a5c6db8ca42c85b1dd7e47667d08cae214
hinata <hinatachikao1971@gmail.com>
2025-04-10 12:50:09 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,4 +1,4 @@
-/*  */
+/* naked reference: auto return type deduction */
 
 #include <iostream>
 #include "../header/utillib.hpp"
@@ -7,8 +7,28 @@
 #define log std::clog
 #define err std::cerr
 
+auto &max(int &a, int &b) noexcept;
+
+auto &max(int &a, int &b) noexcept {
+
+    if (a > b) {
+        return (a);
+    } else {
+        return (b);
+    }
+}
+
 
 int main() {
+
+    int x{120};
+    int y{220};
+
+    int &result1{max(x, y)};
+    int result2{max(x, y)};
+
+    out << "value of result1 is: " << result1 << NL;
+    out << "value of result2 is: " << result2 << NL;
 
 
     std::cout << "\n #(23:36:39): The End ..." << std::endl;
