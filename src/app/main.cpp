4937e71286fae3eef0ae54306ae364c8fc8baae1
hinata <hinatachikao1971@gmail.com>
2025-04-10 17:03:00 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,31 +1,16 @@
 /*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 #define out std::cout
 #define log std::clog
 #define err std::cerr
 
-auto find_max_number(auto x, auto y) noexcept;
-
-auto find_max_number(auto x, auto y) noexcept {
-    if (x > y) {
-        return (x);
-    } else {
-        return (y);
-    }
-}
-
 
 int main() {
 
-    double a{420.0};
-    double b{220};
 
-    auto result{find_max_number(a, b)};
-    out << "value of result is: " << result << NL;
-
-    std::cout << "\n #(12:50:09): The End ..." << std::endl;
+    std::cout << "\n #(17:03:00): The End ..." << std::endl;
     return (EXIT_SUCCESS);
 }
