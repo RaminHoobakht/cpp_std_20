bdd8976680f9840beff914d494aff9ef7ce788a2
hinata <hinatachikao1971@gmail.com>
2025-04-11 14:01:51 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,42 +1,16 @@
-/* function overloading  */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 #define out std::cout
 #define log std::clog
 #define err std::cerr
 
-namespace cpp {
-
-    void say_my_name(std::string &name) noexcept;
-    void say_my_name(std::string name) noexcept;
-
-    void say_my_name(std::string &name) noexcept {
-        out << " (std::string &) function was invoked" << NL;
-        out << " your name is: " << name << NL;
-    }
-
-    void say_my_name(std::string name) noexcept {
-        out << " (std::string name) function was invoked" << NL;
-        out << " your name is: " << name << NL;
-    }
-
-} // namespace cpp
-
 
 int main() {
 
-    LF;
 
-
-    std::string name{"Daniel"};
-
-    /* ambiguous call function occur */
-    cpp::say_my_name(name);
-
-    cpp::say_my_name("Daniel");
-
-    std::cout << "\n #(13:08:24): The End ..." << std::endl;
+    std::cout << "\n #(14:01:51): The End ..." << std::endl;
     return (EXIT_SUCCESS);
 }
