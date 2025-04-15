c526c98bbb285113b634f2e0c052b7a048810571
hinata <hinatachikao1971@gmail.com>
2025-04-16 01:29:53 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -15,9 +15,28 @@
 #define log std::clog
 #define err std::cerr
 
+namespace cpp {
+
+    void say_my_name(const std::string &my_name) noexcept;
+    void say_my_name(std::string my_name) noexcept;
+
+    /* ------------------ */
+
+    void say_my_name(const std::string &my_name) noexcept {
+        out << "your name is: " << my_name << NL;
+    }
+
+    void say_my_name(const std::string my_name) noexcept {
+        out << "your name is: " << my_name << NL;
+    }
+
+
+} // namespace cpp
 
 int main() {
 
+    /* call proper function is ambiguous */
+    cpp::say_my_name("Hinata Chikao");
 
     std::cout << "\n #(17:39:54): The End ..." << std::endl;
     return (EXIT_SUCCESS);
