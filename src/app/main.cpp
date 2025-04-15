955ea4e030175ebf1ff6e1860aa6d601165292f9
hinata <hinatachikao1971@gmail.com>
2025-04-16 02:50:02 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,30 @@
-/*  */
+/* function overloading */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 #define out std::cout << SP
 #define log std::clog << SP
 #define err std::cerr << SP
+
+namespace cpp {
+
+    /* both two function are the same function. */
+    int max(int a, int b) noexcept;
+    int max(const int a, const int b) noexcept;
+
+    int max(int a, int b) noexcept {
+        out << "int max(int, int) was invoked: " << NL;
+        return (a > b) ? a : b;
+    }
+
+    int max(const int a, const int b) noexcept {
+        out << "int max(const int, const int) was invoked: " << NL;
+        return (a > b) ? a : b;
+    }
+
+
+} // namespace cpp
 
 
 int main() {
