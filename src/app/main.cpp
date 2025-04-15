d139b10fca4896056473bdc11972bfb9af0346fb
hinata <hinatachikao1971@gmail.com>
2025-04-16 01:29:53 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,43 +1,16 @@
-/*
-    function overloading
-    overload can be occur by:
+/*  */
 
-    - parameters ordering.
-    - difference number parameters.
-    - changing data-type.
-
-*/
-
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 #define out std::cout
 #define log std::clog
 #define err std::cerr
 
-namespace cpp {
-
-    void say_my_name(const std::string &my_name) noexcept;
-    void say_my_name(std::string my_name) noexcept;
-
-    /* ------------------ */
-
-    void say_my_name(const std::string &my_name) noexcept {
-        out << "your name is: " << my_name << NL;
-    }
-
-    void say_my_name(const std::string my_name) noexcept {
-        out << "your name is: " << my_name << NL;
-    }
-
-
-} // namespace cpp
 
 int main() {
 
-    /* call proper function is ambiguous */
-    cpp::say_my_name("Hinata Chikao");
 
-    std::cout << "\n #(17:39:54): The End ..." << std::endl;
+    std::cout << "\n #(01:29:53): The End ..." << std::endl;
     return (EXIT_SUCCESS);
 }
