4fba0475fc95daf1e57e34b9e4b03d45f26efa48
hinata <hinatachikao1971@gmail.com>
2025-04-16 03:18:01 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,31 @@
-/*  */
+/* function overloading */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 #define out std::cout << SP
 #define log std::clog << SP
 #define err std::cerr << SP
+
+namespace cpp {
+
+    /* both below functions are the same function and
+       can not be overloaded */
+    int max(const int *a, const int *b) noexcept;
+    int max(const int *const a, const int *const b) noexcept;
+
+    int max(const int *a, const int *b) noexcept {
+        out << "int max(const int *a, const int *b) was invoked: " << NL;
+        return (*a > *b) ? *a : *b;
+    }
+
+    int max(const int *const a, const int *const b) noexcept {
+        out << "int max(const int *a, const int *b) was invoked: " << NL;
+        return (*a > *b) ? *a : *b;
+    }
+
+
+} // namespace cpp
 
 
 int main() {
