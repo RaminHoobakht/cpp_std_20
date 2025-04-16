5c77f1bd9378eefd8f9e7c2cd3cfadb3abe4ea6f
hinata <hinatachikao1971@gmail.com>
2025-04-16 22:15:41 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,31 @@
 /*  */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 
 int main() {
+
+    LF;
+
+    int a{10};
+    int b{20};
+
+    auto func1 = [a, b]() {
+        /* code */
+        out << "(a + b) equals: " << (a + b) << NL;
+    };
+
+    func1();
+
+    /* variable a and variable b as default are constant
+       and can not changed */
+    auto func2 = [a, b]() {
+        /* code */
+        ++a;
+        ++b;
+        out << "(a + b) equals: " << (a + b) << NL;
+    };
 
 
     out << "\n #(22:01:35): The End ..." << end;
