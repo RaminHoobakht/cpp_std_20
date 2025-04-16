ecc64fa534bba5b61abfbd89247a1b58145c775d
hinata <hinatachikao1971@gmail.com>
2025-04-16 21:25:23 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,30 @@
-/* lumbda function */
+/* lambda function */
 
 #include <iostream>
 #include "../header/utillib.hpp"
 
 
 int main() {
+
+    auto result = [](double a, double b) noexcept(true) {
+        LF;
+        return (a + b);
+    }(36.52, 47.96);
+
+    out << "value of result is: " << result << NL;
+
+
+    []() {
+        LF;
+        out << "I am a C/C++ Developer ..." << NL;
+    }();
+
+    auto hello = []() {
+        LF;
+        out << "I am a C/C++ Developer ..." << NL;
+    };
+
+    hello();
 
 
     out << "\n #(20:43:24): The End ..." << end;
