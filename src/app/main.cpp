498619b30b3809ac3d7df3d33ad8284ed3ef43e2
hinata <hinatachikao1971@gmail.com>
2025-04-16 09:16:35 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,50 +1,16 @@
-/* function overloading */
+/*  */
 
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 #define out std::cout << SP
 #define log std::clog << SP
 #define err std::cerr << SP
 
-namespace cpp {
-
-    int max(int *a, int *b) noexcept;
-
-    /* both two below functions are the same functions.
-       and can not overloaded */
-    int max(const int *a, const int *b) noexcept;
-    int max(const int *const a, const int *const b) noexcept;
-
-    /* ---------------- */
-
-    int max(int *a, int *b) noexcept {
-        /* code */
-        return (*a > *b) ? *a : *b;
-    }
-
-    int max(const int *a, const int *b) noexcept {
-        /* code */
-        return (*a > *b) ? *a : *b;
-    }
-
-    int max(const int *const a, const int *const b) noexcept {
-        /* code */
-        return (*a > *b) ? *a : *b;
-    }
-
-
-} // namespace cpp
-
 
 int main() {
 
-    int x{25};
-    int y{14};
 
-    auto result{cpp::max(&x, &y)};
-    out << "max is: " << result << NL;
-
-    std::cout << "\n #(03:18:01): The End ..." << std::endl;
+    std::cout << "\n #(09:16:35): The End ..." << std::endl;
     return (EXIT_SUCCESS);
 }
