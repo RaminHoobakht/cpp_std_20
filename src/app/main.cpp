0f624640885f5e6fc48c185012f6af42c4c79988
hinata <hinatachikao1971@gmail.com>
2025-04-16 23:07:22 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,26 @@
-/*  */
+/* lambda function: capture all by reference */
 
-#include "../header/utillib.hpp"
+#include <cstddef>
 #include <iostream>
+#include "../header/utillib.hpp"
 
 
 int main() {
 
+    LF;
+
+    int age{54};
+
+    /* capture all by reference */
+    auto func = [&]() {
+        /* code */
+        out << "value of age is: " << age << NL;
+    };
+
+    for (size_t i{util::zero}; i < 10; ++i) {
+        func();
+        ++age;
+    }
 
     out << "\n #(22:54:21): The End ..." << end;
     return EXIT_SUCCESS;
