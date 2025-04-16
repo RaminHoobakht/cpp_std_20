b93b0ae15cf875620d4239bd1c65faaabad157cd
hinata <hinatachikao1971@gmail.com>
2025-04-16 22:49:53 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,24 @@
-/*  */
+/* lambda function capture by reference */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 
 int main() {
+
+    LF;
+
+    int c{54};
+
+    auto func = [&c]() {
+        /* code */
+        out << "value of c is: " << c << NL;
+    };
+
+    for (size_t i{util::zero}; i < 10; ++i) {
+        func();
+        ++c;
+    }
 
 
     out << "\n #(22:35:38): The End ..." << end;
