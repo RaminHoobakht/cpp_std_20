b7ff2fa4af20865dd110fd2536a70be0ccb1c850
hinata <hinatachikao1971@gmail.com>
2025-04-16 18:00:19 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,14 +1,40 @@
-/*  */
+/* function overloading by default value */
 
-#include "../header/utillib.hpp"
 #include <iostream>
+#include "../header/utillib.hpp"
 
 #define out std::cout << SP
 #define log std::clog << SP
 #define err std::cerr << SP
 
+namespace cpp {
+
+    void print_age(int age = 33) noexcept;
+    void print_age(long age = 54) noexcept;
+
+    void print_age(int age) noexcept {
+        out << "int: your age is : " << age << NL;
+    }
+
+    void print_age(long age) noexcept {
+        out << "long: your age is: " << age << NL;
+    }
+
+
+} // namespace cpp
+
 
 int main() {
+
+    /* call to ambiguous */
+    // cpp::print_age();
+
+    int age1{54};
+    long age2{54};
+
+
+    cpp::print_age(age1);
+    cpp::print_age(age2);
 
 
     std::cout << "\n #(17:47:24): The End ..." << std::endl;
