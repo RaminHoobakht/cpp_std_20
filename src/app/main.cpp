96c6dd298a649b7a5203466f9180ac3a34a7507a
hinata <hinatachikao1971@gmail.com>
2025-04-16 16:03:40 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,46 +1,16 @@
-/*
-    reference of the x variable within main function is deference between
-    within desplay_reference() function. although it passing to the function
-    by reference. I don not why?
+/*  */
 
- */
-
-#include <iostream>
 #include "../header/utillib.hpp"
+#include <iostream>
 
 #define out std::cout << SP
 #define log std::clog << SP
 #define err std::cerr << SP
 
-void display_refrecnce(int *num) noexcept;
 
 int main() {
 
-    LF;
 
-    int x{36};
-
-    out << "main before: value of x variable is: " << x << NL;
-    out << "main before: reference of the x variable is: " << &x << NL;
-    LF_SEP_LF;
-
-    display_refrecnce(&x);
-
-
-    out << "main after: value of x variable is: " << x << NL;
-    out << "main after: reference of the x variable is: " << &x << NL;
-    LF_SEP_LF;
-
-
-    std::cout << "\n #(09:16:35): The End ..." << std::endl;
+    std::cout << "\n #(16:03:40): The End ..." << std::endl;
     return (EXIT_SUCCESS);
-}
-
-void display_refrecnce(int *num) noexcept {
-    out << "dr func before: value of num variable is: " << *num << NL;
-    out << "dr func before: reference of num is: " << &num << NL;
-    *num = 120;
-    out << "dr func after : value of num variable is: " << *num << NL;
-    out << "dr func after : reference of num is: " << &num << NL;
-    LF_SEP_LF;
 }
