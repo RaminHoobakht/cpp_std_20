593c50c9ac8b5a8ac93b1bef3938ef2b97d5850a
hinata <hinatachikao1971@gmail.com>
2025-04-17 12:21:44 +0330

    new cpp source files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,23 @@
-/*   */
+/* recursive function  */
 
 #include "main.hpp"
 
+int add_number(int x) noexcept;
 
 int main() {
 
+    int result{add_number(12)};
+    out << "value of number is: " << result << NL;
 
     out << "\n #(11:57:21): The End ..." << end;
     return EXIT_SUCCESS;
+}
+
+int add_number(int x) noexcept {
+
+    if (x != 0) {
+        return x + add_number(x - 1);
+    }
+
+    return x;
 }
