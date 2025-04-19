3f8588de46891089d69a954c1dd0ec4cdfc15af5
hinata <hinatachikao1971@gmail.com>
2025-04-19 05:49:32 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,26 @@
-/*   */
+/* function template return type deduction with auto  */
 
 #include "main.hpp"
+
+namespace cpp {
+
+    auto maximum(auto a, auto b) noexcept;
+
+    auto maximum(auto a, auto b) noexcept { return (a > b) ? a : b; }
+
+} // namespace cpp
 
 
 int main() {
 
+    double a{10.23};
+    int b{20};
+
+    /* result deduced to the (double) data-type. because it larger than int. 
+     * so value of b variable is returned with double data-type. */
+    auto result{cpp::maximum(a, b)};
+    out << "value of result is: " << result << NL;
+    out << "result type name: " << typeid(result).name() << NL;
 
     out << "\n #(04:57:10): The End ..." << eln;
     return EXIT_SUCCESS;
--- src/app/main.hpp
+++ src/app/main.hpp
@@ -1,6 +1,4 @@
 /*   */
 
+#include <typeinfo>
 #include "../header/utillib.hpp"
-
-
-
