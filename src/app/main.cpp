86d44e6faeb76f2d03b47f5ec2d82a25daa76218
hinata <hinatachikao1971@gmail.com>
2025-04-19 04:57:10 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,10 +2,27 @@
 
 #include "main.hpp"
 
+namespace cpp {
+
+
+    template<typename ReturnType, typename T, typename P>
+    ReturnType maximum(T a, P b) noexcept;
+
+    /* ------------------ */
+
+    template<typename ReturnType, typename T, typename P>
+    ReturnType maximum(T a, P b) noexcept {
+        return (a > b) ? a : b;
+    }
+
+
+} // namespace cpp
+
+
 int main() {
 
-
-
+    auto result1{cpp::maximum<int>(120, 220)};
+    out << "value of result1 is: " << result1 << NL;
 
     out << "\n #(23:54:00): The End ..." << eln;
     return EXIT_SUCCESS;
