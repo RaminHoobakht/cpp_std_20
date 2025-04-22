5943b64a60357caa0bf992c69aa47dc86d9e5543
hinata <hinatachikao1971@gmail.com>
2025-04-22 12:56:31 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,38 +1,11 @@
-/* using namespace in down and up the main function */
-
+/*   */
 
 #include "main.hpp"
 
 
-/* it can be divide namespace in two part
-    one part for function declaration and the 
-    other one for function definition. */
-
-namespace cpp {
-
-    template<typename T>
-    T maximum(T a, T b) noexcept;
-
-}
-
-
 int main() {
 
-    int a{120};
-    int b{320};
 
-    auto result{cpp::maximum(a, b)};
-    out << "maximum is: " << result << NL;
-
-    out << "\n #(02:20:03): The End ..." << eln;
+    out << "\n #(12:56:31): The End ..." << eln;
     return EXIT_SUCCESS;
 }
-
-namespace cpp {
-
-    template<typename T>
-    T maximum(T a, T b) noexcept {
-        return (a > b) ? a : b;
-    }
-
-} // namespace cpp
