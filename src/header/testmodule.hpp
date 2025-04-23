f4dde464b334e39033b45da0520644f4cb810868
hinata <hinatachikao1971@gmail.com>
2025-04-23 21:06:34 +0330

    before creating new source file

--- src/header/testmodule.hpp
+++ src/header/testmodule.hpp
@@ -1,10 +1,24 @@
 #ifndef HEADER_TEST_MODULE_HPP
 #define HEADER_TEST_MODULE_HPP
 
+#include <cstring>
+
 /* declaration */
 namespace cpp {
 
-  
+    template<typename T>
+    T maximum(T a, T b) noexcept;
+
+    template<>
+    const char *maximum(const char *a, const char *b) noexcept;
+
+    /* ---------------------- */
+
+    template<typename T>
+    T maximum(T a, T b) noexcept {
+        return (a > b) ? a : b;
+    }
+
 
 } // namespace cpp
 
