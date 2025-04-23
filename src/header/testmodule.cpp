9ce6daa80feaba6220d56bdc7db4700c4c9b67e6
hinata <hinatachikao1971@gmail.com>
2025-04-24 01:49:12 +0330

    after creating new source file

--- src/header/testmodule.cpp
+++ src/header/testmodule.cpp
@@ -3,10 +3,9 @@
 /* definition */
 namespace cpp {
 
-    template<>
-    const char *maximum(const char *a, const char *b) noexcept {
-        return (std::strcmp(a, b) > 0) ? a : b;
-    }
 
 
 } // namespace cpp
+
+
+
