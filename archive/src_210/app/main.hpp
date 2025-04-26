465498296d14f10168b269dc4250a81efeb84ab4
hinata <hinatachikao1971@gmail.com>
2025-04-26 03:47:20 +0330

    after creating new source file

--- archive/src_210/app/main.hpp
+++ archive/src_210/app/main.hpp
@@ -4,6 +4,3 @@
 
 #include "../header/testmodule.hpp"
 #include "../header/utillib.hpp"
-
-
-
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,19 +1,11 @@
-/* Subject: decltype and trailing return types  */
+/*   */
 
 #include "main.hpp"
 
 
 int main() {
 
-    int a{25};
-    double b{13.36};
 
-    decltype((a > b) ? a : b) c{};
-
-    auto result = util::data_type_name(c);
-    out << "data type is: " << result << NL;
-
-
-    out << "\n #(02:49:14): The End ..." << eln;
+    out << "\n #(03:47:20): The End ..." << eln;
     return EXIT_SUCCESS;
 }
