0d1d13be40bbcb1430cd04a34efe76f4307d7f4e
hinata <hinatachikao1971@gmail.com>
2025-04-28 15:25:13 +0330

    after creating new source file

---
+++ archive/src_222/app/main.cpp
@@ -0,0 +1,35 @@
+/* Subject: using concepts  */
+
+#include "main.hpp"
+
+int main() {
+    constexpr int a{20};
+    constexpr int b{30};
+
+    const auto result_one{cpp::add_one(a, b)};
+    out << "value of result one is: " << result_one << NL;
+    SEP;
+
+    const auto result_two{cpp::add_two(a, b)};
+    out << "value of result two is: " << result_two << NL;
+    SEP;
+
+    const auto result_three{cpp::add_three(a, b)};
+    out << "value of result three is: " << result_three << NL;
+    SEP;
+
+    const auto result_four{cpp::add_four(a, b)};
+    out << "value of result four is: " << result_four << NL;
+    SEP;
+
+    const auto result_five{cpp::add_five(a, b)};
+    out << "value of result five is: " << result_five << NL;
+    SEP;
+
+    const auto result_six{cpp::add_five(a, b)};
+    out << "value of result six is: " << result_six << NL;
+    SEP;
+
+    out << "\n #(04:19:44): The End ..." << eln;
+    return EXIT_SUCCESS;
+}
