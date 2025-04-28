3f6c71bca0651bffca0005f6a73f127eb39d64e3
hinata <hinatachikao1971@gmail.com>
2025-04-28 04:19:43 +0330

    before creating new source file

--- src/header/testmodule.hpp
+++ src/header/testmodule.hpp
@@ -1,9 +1,24 @@
 #ifndef HEADER_TEST_MODULE_HPP
 #define HEADER_TEST_MODULE_HPP
 
+#include <concepts>
+
+
 /* declaration */
 namespace cpp {
 
+
+    template<typename T>
+        requires std::integral<T>
+    T add(T a, T b) noexcept;
+
+    /* ----------------- */
+
+    template<typename T>
+        requires std::integral<T>
+    T add(T a, T b) noexcept {
+        return a + b;
+    }
 
 
 } // namespace cpp
