8f2881e7dbb415b3b1bfd2e9e05ce3daf07cb36e
hinata <hinatachikao1971@gmail.com>
2025-05-02 23:08:06 +0330

    new cpp source files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,7 +5,6 @@
 
 int main() {
 
-
-    out << "\n #(23:03:29): The End ..." << eln;
+    out << "\n #(00:00:00): The End ..." << eln;
     return EXIT_SUCCESS;
 }
--- src/app/main.hpp
+++ src/app/main.hpp
@@ -1,9 +1,8 @@
-/*   */
+/* Header:   */
 
 #pragma once
 
 #include "../header/testmodule.hpp"
 #include "../header/utillib.hpp"
-
 
 
