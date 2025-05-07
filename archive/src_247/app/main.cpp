3c22f0fa2631809ba88b06fbf5d90f3f47a1b36e
hinata <hinatachikao1971@gmail.com>
2025-05-08 01:42:44 +0330

    after creating new source file

---
+++ archive/src_247/app/main.cpp
@@ -0,0 +1,18 @@
+/* Subject: implementing person class with all constructors and
+            constructor delegation, copy and move constructor,
+            operator overloading.  */
+
+#include "main.hpp"
+
+
+int main() {
+
+    LF;
+
+    cpp::person hinata{"Hinata", "Chikao", 1001, 54};
+
+    out << hinata << NL;
+
+    out << "\n #(21:41:14): The End ..." << eln;
+    return EXIT_SUCCESS;
+}
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,18 +1,12 @@
-/* Subject: implementing person class with all constructors and
-            constructor delegation, copy and move constructor,
-            operator overloading.  */
+/* Subject:   */
 
 #include "main.hpp"
 
 
 int main() {
 
-    LF;
+	LF;
 
-    cpp::person hinata{"Hinata", "Chikao", 1001, 54};
-
-    out << hinata << NL;
-
-    out << "\n #(21:41:14): The End ..." << eln;
+    out << "\n #(01:42:44): The End ..." << eln;
     return EXIT_SUCCESS;
 }
