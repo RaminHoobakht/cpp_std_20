cbe4ae20cfe532aaf5fdff3b42d4ee8438c8676f
hinata <hinatachikao1971@gmail.com>
2025-05-10 02:56:29 +0330

    new cpp source file for friend function

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,11 +2,17 @@
 
 #include "main.hpp"
 
-using component = entity::component;
+using namespace entity;
 
 int main() {
 
     LF;
+
+    dog my_dog{"jes", "Golden", 7};
+    display_dog_info(my_dog);
+    SEP;
+
+    //display_dog_info_();
 
     out << "\n #(19:27:08): The End ..." << eln;
     return EXIT_SUCCESS;
