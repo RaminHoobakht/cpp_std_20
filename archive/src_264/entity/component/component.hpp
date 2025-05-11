476d653414d435fe1462d8cc40061992a85d6046
hinata <hinatachikao1971@gmail.com>
2025-05-11 21:02:25 +0330

    after creating new source file

---
+++ archive/src_264/entity/component/component.hpp
@@ -0,0 +1,16 @@
+#ifndef COMPONENT_HPP
+#define COMPONENT_HPP
+
+#pragma once
+
+namespace entity {
+
+    struct component {
+        double x{};
+        double y{};
+        double z{};
+    };
+
+} // namespace entity
+
+#endif
