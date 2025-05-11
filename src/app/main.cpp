05643ff151a19909cdfc7104bce3caceb2b31a95
hinata <hinatachikao1971@gmail.com>
2025-05-11 20:47:19 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,36 +7,38 @@ namespace cpp {
     class one final {
 
     private:
-        /* A const variable: it can initial in header and in constructor
-           by initializer list. but not any other place line in setter function.
+        /* A static variable: it cannot initial in header and in constructor
+           by initializer list. it must be initial in somewhere out of class.
          */
-        const int x_{15};
+        static int x_;
 
     public:
         [[nodiscard]] explicit one(const int x_param) noexcept;
         ~one() noexcept;
-        // inline void set_x(int x_param) noexcept;
+        inline void set_x(int x_param) noexcept;
         [[nodiscard]] inline int get_x() const noexcept;
     };
 
 
-    one::one(const int x_param) noexcept : x_{x_param} {
+    one::one(const int x_param) noexcept {
         /* code */
+        one::x_ = x_param;
         out << "class one constructor ..." << NL;
     }
 
     one::~one() noexcept { out << "Goodbye ..." << NL; }
 
-    // inline void one::set_x(int x_param) noexcept {
-    //     /* code  */
-    //     x_ = x_param;
-    // }
+    inline void one::set_x(int x_param) noexcept {
+        /* code  */
+        one::x_ = x_param;
+    }
 
     inline int one::get_x() const noexcept {
         /* code */
-        return x_;
+        return one::x_;
     }
 
+    int cpp::one::x_{0};
 
 } // namespace cpp
 
@@ -47,8 +49,6 @@ int main() {
     cpp::one my_one{120};
     out << my_one.get_x() << NL;
 
-    out << "\n #(20:36:20): The End ..." << eln;
+    out << "\n #(20:40:45): The End ..." << eln;
     return EXIT_SUCCESS;
 }
-
-//(20:40:45)
