3393547ef5729c1c28c3315ff09132c1687855e7
hinata <hinatachikao1971@gmail.com>
2025-05-11 20:40:45 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,13 +7,15 @@ namespace cpp {
     class one final {
 
     private:
-        /* a normal variable */
-        int x_{};
+        /* A const variable: it can initial in header and in constructor
+           by initializer list. but not any other place line in setter function.
+         */
+        const int x_{15};
 
     public:
         [[nodiscard]] explicit one(const int x_param) noexcept;
         ~one() noexcept;
-        inline void set_x(int x_param) noexcept;
+        // inline void set_x(int x_param) noexcept;
         [[nodiscard]] inline int get_x() const noexcept;
     };
 
@@ -25,10 +27,10 @@ namespace cpp {
 
     one::~one() noexcept { out << "Goodbye ..." << NL; }
 
-    inline void one::set_x(int x_param) noexcept {
-        /* code  */
-        x_ = x_param;
-    }
+    // inline void one::set_x(int x_param) noexcept {
+    //     /* code  */
+    //     x_ = x_param;
+    // }
 
     inline int one::get_x() const noexcept {
         /* code */
@@ -45,8 +47,6 @@ int main() {
     cpp::one my_one{120};
     out << my_one.get_x() << NL;
 
-    out << "\n #(18:09:03): The End ..." << eln;
+    out << "\n #(20:36:20): The End ..." << eln;
     return EXIT_SUCCESS;
 }
-
-//(20:36:20)
