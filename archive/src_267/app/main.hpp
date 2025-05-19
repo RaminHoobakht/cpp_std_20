8085262bc157b4427ebc4d3029eb9beb0dbb41ba
hinata <hinatachikao1971@gmail.com>
2025-05-19 17:14:07 +0330

    after creating new source file

---
+++ archive/src_267/app/main.hpp
@@ -0,0 +1,9 @@
+#ifndef HEADER_MAIN_HPP
+#define HEADER_MAIN_HPP
+
+#pragma once
+
+#include "../entity/cylinder/cylinder.hpp"
+#include "../util/util.hpp"
+
+#endif /* HEADER_MAIN_HPP */
---
+++ archive/src_267/entity/user/user_headers.hpp
@@ -0,0 +1,11 @@
+#ifndef SRC_ENTITY_USER_USER_HEADERS_HPP_
+#define SRC_ENTITY_USER_USER_HEADERS_HPP_
+
+#pragma once
+
+#include <string>
+#include <string_view>
+#include "../../util/utillib.hpp"
+
+
+#endif /* SRC_ENTITY_USER_USER_HEADERS_HPP_ */
