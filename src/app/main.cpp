4516723394c3a10e83db684b2ed63f2d7f12b129
hinata <hinatachikao1971@gmail.com>
2025-05-28 08:37:26 +0330

    new cpp files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,13 @@
+/*
+ *  Subject:
+ *
+ * */
 
 #include "main.hpp"
 
 
 int main() {
 
-    pout << "\n #(00:26:37): The End ..." << eln;
+    pout << "\n #(08:34:15): The End ..." << eln;
     return EXIT_SUCCESS;
 }
