9d13e96f2a2c89acb023f8f024b2c9e79f156cdb
hinata <hinatachikao1971@gmail.com>
2025-06-03 00:34:18 +0330

    before creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,6 @@
 /*
  *  Subject: Using Smart Pointer:
- *              - using unique smart pointer with array
+ *              - using shared smart pointer with arrays
  *
  * */
 
@@ -15,6 +15,21 @@ int main() {
 
     LF;
 
+    std::shared_ptr<int[]> int_array{new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}};
+
+    for (int i{0}; i < 9; ++i) {
+        pout << int_array[i] << NL;
+    }
+
+    std::shared_ptr<int[]> int_array2{std::make_shared<int[]>(10)};
+
+    for (int i{0}; i < 10; i++) {
+        int_array2[i] = i;
+    }
+
+    for (int i{0}; i < 10; i++) {
+        pout << int_array2[i] << NL;
+    }
 
     pout << "\n #(20:33:08): The End ..." << eln;
     return EXIT_SUCCESS;
