52d30ee620b9434a8e5c5ac65b843d31675bf8d3
hinata <hinatachikao1971@gmail.com>
2025-06-06 03:17:27 +0330

    clear old code

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -11,27 +11,6 @@ int main() {
 
     LF;
 
-    point point_one{120.36, 71.14};
-    SEP;
-
-    point_one.print_info();
-    SEP;
-
-    point point_two{15.85, 41.74};
-    SEP;
-
-    point_two.print_info();
-    SEP;
-
-    point point_three{point_one + point_two};
-    SEP;
-
-    point_three.print_info();
-    SEP;
-
-
-    (point_one + point_two + point_three).print_info();
-
 
     pout << "\n #(06:43:27): The End ..." << eln;
     return EXIT_SUCCESS;
