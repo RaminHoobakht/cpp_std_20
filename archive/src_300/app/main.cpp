d017cb780d1c1ed7a7c31edae93d2b95baf156a0
hinata <hinatachikao1971@gmail.com>
2025-06-06 03:16:52 +0330

    after creating new source file

--- archive/src_300/app/main.cpp
+++ archive/src_300/app/main.cpp
@@ -36,5 +36,3 @@ int main() {
     pout << "\n #(06:43:27): The End ..." << eln;
     return EXIT_SUCCESS;
 }
-
-//(03:16:52)
---
+++ archive/src_300/entity/point3/point3_headers.hpp
@@ -0,0 +1,12 @@
+#ifndef HEADER_POINT3_HEADERS_HPP
+#define HEADER_POINT3_HEADERS_HPP
+
+#pragma once
+
+#include <cmath>
+#include <sstream>
+#include <utility>
+#include "../../util/util.hpp"
+
+
+#endif
