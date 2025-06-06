47b905306d6f4f7b805273cfee833c955a4f7de6
hinata <hinatachikao1971@gmail.com>
2025-06-06 22:04:10 +0330

    new source

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,5 +1,5 @@
 /*
- *  Subject: operator overloading ([])
+ *  Subject: operator overloading
  *
  * */
 
@@ -10,15 +10,7 @@ using point = entity::point3;
 
 int main() {
 
-    point my_point{123.654, 789.654};
 
-    pout << "value of x point is: " << my_point[0] << NL;
-    pout << "value of y point is: " << my_point[1] << NL;
-    
-    // pout << "value of y point is: " << my_point[2] << NL;
-
-    pout << "\n #(20:19:58): The End ..." << eln;
+    pout << "\n #(22:03:21): The End ..." << eln;
     return EXIT_SUCCESS;
 }
-
-//(22:03:21)
