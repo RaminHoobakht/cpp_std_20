d1ca38eee93f683289a1e8291154f4662f95b859
hinata <hinatachikao1971@gmail.com>
2025-06-06 20:21:02 +0330

    clear old code

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -9,27 +9,7 @@ using point = entity::point3;
 
 int main() {
 
-    pout << "create point one: " << NL;
-    point point_one{120.36, 96.52};
-    point_one.print_info();
-    SEP;
 
-
-    pout << "create point two: " << NL;
-    point point_two{87.12, 54.21};
-    point_two.print_info();
-    SEP;
-
-    pout << "create point three: " << NL;
-    point point_three{point_one + point_two};
-    SEP;
-
-    pout << "print information for point three: " << NL;
-    point_three.print_info();
-    SEP;
-
-    pout << "\n #(04:52:38): The End ..." << eln;
+    pout << "\n #(20:19:58): The End ..." << eln;
     return EXIT_SUCCESS;
 }
-
-//(20:19:58)
